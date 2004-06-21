3fe2e451c7e8fa6ba68ae6d69964dc2531b554f5
Elliot Wolin <wolin@jlab.org>
2004-06-21 18:31:48 +0000

    Now need Carl's stuff
    2004/06/18 16:36:18-04:00 jlab.org!wolin
    Still working...
    
    2004/06/17 14:14:03-04:00 jlab.org!wolin
    Continued improvements
    
    2004/06/15 11:31:35-04:00 jlab.org!wolin
    Still working on return codes
    
    2004/06/14 14:55:28-04:00 jlab.org!wolin
    Still playing with return codes
    
    2004/06/14 12:48:58-04:00 jlab.org!wolin
    Added ifdef protection and extern c stuff
    
    2004/06/14 12:43:23-04:00 jlab.org!wolin
    
    (Logical change 1.3)

---
+++ trunk/cMsg/cMsg.h
@@ -0,0 +1,99 @@
+/*
+ *  cMsg.h
+ *
+ *  Defines cMsg API, data structures, and return codes
+ *
+ *  E.Wolin, 14-Jun-2004, Jefferson Lab
+ *
+ *
+ *  Notes:
+ *    user must always free the message
+ *    can have many callbacks for the same subject and type
+ *    each callback executes in its own thread
+ *    flush may happen before cMsgFlush call
+ *
+ */
+
+#ifndef _cMsg_h
+#define _cMsg_h
+
+
+/* required includes */
+#include <time.h>
+
+
+/* cMsg message structure */
+typedef struct cMsg {
+  int sysMsgId;
+  char *sender;
+  int senderId;
+  char *senderHost;
+  time_t senderTime;
+  int senderMsgId;
+  char *receiver;
+  char *receiverHost;
+  time_t receiverTime;
+  char *project;
+  char *subject;
+  char *type;
+  char *text;
+};
+
+
+/* cMsg message callback */
+typedef void cMsgCallback(cMsg *msg, void *userArg);
+
+
+/* cMsg function prototypes */
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+  int cMsgInit(char *myProject, char *myName, char *myDescription);
+  int cMsgSend(char *subject, char *type, char *text);
+  int cMsgFlush(void);
+  int cMsgGet(char *subject, char *type, time_t timeout, cMsg **msg);
+  int cMsgSubscribe(char *subject, char *type, cMsgCallback *callback, void *userArg);
+  int cMsgUnSubscribe(char *subject, char *type, cMsgCallback *callback);
+  int cMsgReceiveStart(void);
+  int cMsgReceiveStop(void);
+  int cMsgFree(cMsg *msg);
+  int cMsgDone(void);
+  int cMsgPerror(int error);
+  
+
+  char*     cMsgGetProject();
+  char*     cMsgGetName();
+  char*     cMsgGetDescription();
+  char*     cMsgGetHost();
+  int 	    cMsgGetSendSocket();
+  int 	    cMsgGetReceiveSocket();
+  pthread_t cMsgGetPendThread();
+  int 	    cMsgGetInitState();
+  int 	    cMsgGetReceiveState();
+
+
+#ifdef __cplusplus
+}
+#endif
+
+
+/* cMsg return codes */
+enum {
+  CMSG_OK              	  = 0,
+  CMSG_ERROR,
+  CMSG_NOT_IMPLEMENTED,
+  CMSG_BAD_ARGUMENT,
+  CMSG_NAME_EXISTS,
+  CMSG_NOT_INITIALIZED,
+  CMSG_ALREADY_INIT,
+  CMSG_LOST_CONNECTION,
+  CMSG_TIMEOUT,
+  CMSG_NETWORK_ERROR,
+  CMSG_PEND_ERROR,
+  CMSG_ILLEGAL_MSGTYPE,
+  CMSG_OUT_OF_MEMORY,
+};
+
+
+#endif /* _cMsg_h */
