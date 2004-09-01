9f9e645c239f1881fd3707ec8b11d675fe0008d0
Carl Timmer <timmer@jlab.org>
2004-09-01 17:18:45 +0000

    add javadoc and more methods
    (Logical change 1.37)

--- trunk/cMsg/org/jlab/coda/cMsg/cMsgHandleRequests.java
+++ trunk/cMsg/org/jlab/coda/cMsg/cMsgHandleRequests.java
@@ -5,7 +5,7 @@
  *    This software was developed under a United States Government license    *
  *    described in the NOTICE file included as part of this distribution.     *
  *                                                                            *
- *    C. Timmer, 7-Jul-2004, Jefferson Lab                                    *
+ *    C. Timmer, 14-Jul-2004, Jefferson Lab                                    *
  *                                                                            *
  *     Author: Carl Timmer                                                    *
  *             timmer@jlab.org                   Jefferson Lab, MS-6B         *
@@ -17,37 +17,93 @@
 package org.jlab.coda.cMsg;
 
 /**
- * This interface is for an object that a domain server uses to respond to client demands.
+ * This interface is an API for an object that a domain server
+ * uses to respond to client demands.
  *
- *  Created by IntelliJ IDEA.
- * User: timmer
- * Date: Jul 14, 2004
- * Time: 1:16:47 PM
+ * @author Carl Timmer
+ * @version 1.0
  */
 public interface cMsgHandleRequests {
-    /** Method to handle message sent by doman client. */
+    /**
+     * Method to register domain client.
+     *
+     * @param name name of client
+     * @param host host client is running on
+     * @param port port client is listening on
+     * @throws cMsgException
+     */
     public void registerClient(String name, String host, int port) throws cMsgException;
 
-    /** Method to handle message sent by doman client. */
-    public void unregisterClient(String name);
+    /**
+     * Method to unregister domain client.
+     *
+     * @param name name of client
+     * @throws cMsgException
+     */
+    public void unregisterClient(String name) throws cMsgException;
 
-    /** Method to handle message sent by doman client. */
+    /**
+     * Method to see if domain client is registered.
+     *
+     * @param name name of client
+     * @return true if client registered, false otherwise
+     * @throws cMsgException
+     */
+    public boolean isRegistered(String name) throws cMsgException;
+
+    /**
+     * Method to handle message sent by domain client.
+     *
+     * @param name name of client
+     * @param msg message from sender
+     * @throws cMsgException
+     */
     public void handleSendRequest(String name, cMsgMessage msg) throws cMsgException;
 
-    /** Method to handle subscribe request sent by doman client. */
+    /**
+      * Method to handle subscribe request sent by domain client.
+      *
+      * @param name name of client
+      * @param subject message subject to subscribe to
+      * @param type message type to subscribe to
+      * @param receiverSubscribeId message id refering to these specific subject and type values
+      * @throws cMsgException
+      */
     public void handleSubscribeRequest(String name, String subject, String type,
                                        int receiverSubscribeId) throws cMsgException;
 
-    /** Method to handle unsubscribe request sent by doman client. */
+    /**
+     * Method to handle sunsubscribe request sent by domain client.
+     *
+     * @param name name of client
+     * @param subject message subject subscribed to
+     * @param type message type subscribed to
+     * @throws cMsgException
+     */
     public void handleUnsubscribeRequest(String name, String subject, String type) throws cMsgException;
 
-    /** Method to handle keepalive sent by doman client
-     *  checking to see if the socket is still up. */
-    public void handleKeepAlive(String name);
+    /**
+     * Method to handle keepalive sent by domain client checking to see
+     * if the domain server socket is still up.
+     *
+     * @param name name of client
+     * @throws cMsgException
+     */
+    public void handleKeepAlive(String name) throws cMsgException;
 
-    /** Method to handle a disconnect request sent by doman client. */
-    public void handleDisconnect(String name);
+    /**
+     * Method to handle a disconnect request sent by domain client.
+     *
+     * @param name name of client
+     * @throws cMsgException
+     */
+    public void handleDisconnect(String name) throws cMsgException;
 
-    /** Method to handle a shutdown request sent by doman client. */
-    public void handleShutdown(String name);
+    /**
+     * Method to handle a request sent by domain client to shut the domain server down.
+     *
+     * @param name name of client
+     * @throws cMsgException
+     */
+    public void handleShutdown(String name) throws cMsgException;
 }
