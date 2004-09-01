a02ac772ad8d6c6fb3ec379b5aec01446a261fbd
Carl Timmer <timmer@jlab.org>
2004-09-01 17:18:45 +0000

    change boilerplate
    (Logical change 1.37)

--- trunk/cMsg/org/jlab/coda/cMsg/coda/JgetenvException.java
+++ trunk/cMsg/org/jlab/coda/cMsg/coda/JgetenvException.java
@@ -1,16 +1,19 @@
 /*---------------------------------------------------------------------------*
-*  Copyright (c) 2003        Southeastern Universities Research Association, *
-*                            Thomas Jefferson National Accelerator Facility  *
-*                                                                            *
-*    This software was developed under a United States Government license    *
-*    described in the NOTICE file included as part of this distribution.     *
-*                                                                            *
-*    Author:  Vardan Gyurjyan                                                *
-*             gurjyan@jlab.org                  Jefferson Lab, MS-12H        *
-*             Phone: (757) 269-5879             12000 Jefferson Ave.         *
-*             Fax:   (757) 269-5800             Newport News, VA 23606       *
-*                                                                            *
-*----------------------------------------------------------------------------*/
+ *  Copyright (c) 2003        Southeastern Universities Research Association, *
+ *                            Thomas Jefferson National Accelerator Facility  *
+ *                                                                            *
+ *    This software was developed under a United States Government license    *
+ *    described in the NOTICE file included as part of this distribution.     *
+ *                                                                            *
+ *    V. Gyurjyan, 12-Jan-2004, Jefferson Lab                                 *
+ *                                                                            *
+ *    Author:  Vardan Gyurjyan                                                *
+ *             gurjyan@jlab.org                  Jefferson Lab, MS-12H        *
+ *             Phone: (757) 269-5879             12000 Jefferson Ave.         *
+ *             Fax:   (757) 269-5800             Newport News, VA 23606       *
+ *                                                                            *
+ *----------------------------------------------------------------------------*/
+
 package org.jlab.coda.cMsg.coda;
 
 /**
