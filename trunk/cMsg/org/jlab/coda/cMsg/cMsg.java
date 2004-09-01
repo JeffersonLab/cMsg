3fb29f47091b93ab0e9b848aad96a8b94608554b
Carl Timmer <timmer@jlab.org>
2004-09-01 17:18:45 +0000

    add javadoc, remove unused methods
    (Logical change 1.37)

--- trunk/cMsg/org/jlab/coda/cMsg/cMsg.java
+++ trunk/cMsg/org/jlab/coda/cMsg/cMsg.java
@@ -18,62 +18,137 @@
 *             Fax:   (757) 269-5800             Newport News, VA 23606       *
 *                                                                            *
 *----------------------------------------------------------------------------*/
-//  implements cMsg API for CODA domain
 
 package org.jlab.coda.cMsg;
 
 import java.lang.*;
 
 
-
+/**
+ * This interface is the cMsg client API for all domains.
+ *
+ * @author Elliott Wolin
+ * @author Carl Timmer
+ * @version 1.0
+ */
 public interface cMsg {
 
-    public int send(String subject, String type, String text);
+    /**
+     * Method to connect to a particular domain server.
+     *
+     * @throws cMsgException
+     */
+    public void connect() throws cMsgException;
+
+    /**
+     * Method to close the connection to the domain server. This method results in this object
+     * becoming functionally useless.
+     *
+     * @throws cMsgException
+     */
+    public void disconnect() throws cMsgException;
+
+    /**
+     * Method to determine if this object is still connected to the domain server or not.
+     *
+     * @return true if connected to domain server, false otherwise
+     */
+    public boolean isConnected();
+
+    /**
+     * Method to send a message to the domain server for further distribution.
+     *
+     * @param message message
+     * @throws cMsgException
+     */
+    public void send(cMsgMessage message) throws cMsgException;
+
+    /**
+     * Method to force cMsg client to send pending communications with domain server.
+     * @throws cMsgException
+     */
+    public void flush() throws cMsgException;
+
+    /**
+     * Method to get a message for a particular subject and type from the domain server.
+     *
+     * @param subject message subject
+     * @param type    message type
+     * @param timeout time in milliseconds to wait for a message
+     * @param msg     object used to hold retrieved message
+     * @throws cMsgException
+     */
+    public void get(String subject, String type, int timeout, cMsgMessage msg) throws cMsgException;
+
+    /**
+     * Method to subscribe to receive messages of a subject and type from the domain server.
+     *
+     * @param subject message subject
+     * @param type    message type
+     * @param cb      callback object whose single method is called upon receiving a message
+     *                of subject and type
+     * @param userObj any user-supplied object to be given to the callback method as an argument
+     * @throws cMsgException
+     */
+    public void subscribe(String subject, String type, cMsgCallback cb, Object userObj) throws cMsgException;
 
 
-    public int flush();
+    /**
+     * Method to unsubscribe a previous subscription to receive messages of a subject and type
+     * from the domain server. Since many subscriptions may be made to the same subject and type
+     * values, but with different callabacks, the callback must be specified so the correct
+     * subscription can be removed.
+     *
+     * @param subject message subject
+     * @param type    message type
+     * @param cb      callback object whose single method is called upon receiving a message
+     *                of subject and type
+     * @throws cMsgException
+     */
+    public void unsubscribe(String subject, String type, cMsgCallback cb) throws cMsgException;
 
+    /**
+     * Method to start or activate the subscription callbacks.
+     *
+     * @throws cMsgException
+     */
+    public void start() throws cMsgException;
 
-    public int get(String subject, String type, int timeout, cMsgMessage msg);
+    /**
+     * Method to stop or deactivate the subscription callbacks.
+     *
+     * @throws cMsgException
+     */
+    public void stop() throws cMsgException;
 
-
-    public int subscribe(String subject, String type, cMsgCallback cb, Object userObj);
-
-
-    public int subscribe(String subject, String type, cMsgCallback cb);
-
-
-    public int done();
-
-
-    public int start();
-
-
-    public int stop();
-
-
+    /**
+     * Get the name of the domain connected to.
+     * @return domain name
+     */
     public String getDomain();
 
-
+    /**
+     * Get the name of the client.
+     * @return client's name
+     */
     public String getName();
 
-
+    /**
+     * Get the client's description.
+     * @return client's description
+     */
     public String getDescription();
 
-
+    /**
+     * Get the host the client is running on.
+     * @return client's host
+     */
     public String getHost();
 
-
-    public int getSendSocket();
-
-
-    public int getReceiveSocket();
-
-
-    public int getPendThread();
-
-
-    public boolean getReceiveState();
-
-
+    /**
+     * Get boolean tells whether callbacks are activated or not. The
+     * start and stop methods activate and deactivate the callbacks.
+     * @return true if callbacks are activated, false if they are not
+     */
+    public boolean isReceiving();
 }
--- trunk/cMsg/org/jlab/coda/cMsg/cMsgImpl.java
+++ trunk/cMsg/org/jlab/coda/cMsg/cMsgImpl.java
@@ -1,165 +1,264 @@
+/*----------------------------------------------------------------------------*
+ *  Copyright (c) 2004        Southeastern Universities Research Association, *
+ *                            Thomas Jefferson National Accelerator Facility  *
+ *                                                                            *
+ *    This software was developed under a United States Government license    *
+ *    described in the NOTICE file included as part of this distribution.     *
+ *                                                                            *
+ *    C. Timmer, 14-Jul-2004, Jefferson Lab                                    *
+ *                                                                            *
+ *     Author: Carl Timmer                                                    *
+ *             timmer@jlab.org                   Jefferson Lab, MS-6B         *
+ *             Phone: (757) 269-5130             12000 Jefferson Ave.         *
+ *             Fax:   (757) 269-5800             Newport News, VA 23606       *
+ *                                                                            *
+ *----------------------------------------------------------------------------*/
+
 package org.jlab.coda.cMsg;
 
 /**
- * Created by IntelliJ IDEA.
- * User: timmer
- * Date: Jul 14, 2004
- * Time: 11:35:31 AM
- * To change this template use File | Settings | File Templates.
+ * This class provides a very basic (non-functional/dummy) implementation of the cMsg interface.
+ * Its non-getter/setter methods throw a cMsgException saying that the method is not implemented.
+ * It is like the swing Adapter classes.
+ *
+ * @author Carl Timmer
+ * @version 1.0
  */
 public class cMsgImpl implements cMsg {
-    // instance variables (is private needed?)
-    protected boolean initialized  = false;
-    protected String domain        = null;
-    protected String name          = null;
-    protected String description   = null;
-    protected int sendSocket       = 0;
-    protected int receiveSocket    = 0;
-    protected int pendThread       = 0;
-    protected String host          = null;
-    protected boolean receiveState = false;
 
+    /** Boolean indicating whether this client is connected to the domain server or not. */
+    protected volatile boolean connected;
+
+    /** Boolean indicating whether this client's callbacks are active or not. */
+    protected boolean receiving;
+
+    /**
+     * The Uniform Domain Locator which tells the location of a name server. It is of the
+     * form domain://host:port/remainder
+     */
+    protected String  UDL;
+
+    /** String containing the remainder part of the UDL. */
+    protected String  UDLremainder;
+
+    /** Domain being connected to. */
+    protected String  domain;
+
+    /** Name of this client. Must be unique in the domain. */
+    protected String  name;
+
+    /** Description of the client. */
+    protected String  description;
+
+    /** Host the client is running on. */
+    protected String  host;
 
 
 //-----------------------------------------------------------------------------
 
 
+    /**
+     * Method to determine if this object is still connected to the domain server or not.
+     *
+     * @return true if connected to domain server, false otherwise
+     */
+    public boolean isConnected() {
+        return connected;
+    }
+
+
+//-----------------------------------------------------------------------------
+
+    /**
+     * Constructor.
+     *
+     * @throws cMsgException always throws an exception since this is a dummy implementation
+     */
     public cMsgImpl() throws cMsgException {
-        this(null, null, null);
+        throw new cMsgException("cMsg is not implemented yet");
     }
 
 
 //-----------------------------------------------------------------------------
 
 
-    public cMsgImpl(String myDomain, String myName, String myDescription) throws cMsgException {
-
-        if (domain.equalsIgnoreCase("CODA")) {
-            domain = myDomain;
-            name = myName;
-            description = myDescription;
-        }
-        else {
-            throw new cMsgException("cMsg is not implemented yet");
-        }
-
-        initialized = true;
+    /**
+     * Method to connect to the domain server.
+     *
+     * @throws cMsgException always throws an exception since this is a dummy implementation
+     */
+    public void connect() throws cMsgException {
+        throw new cMsgException("connect is not implemented yet");
     }
 
 
 //-----------------------------------------------------------------------------
 
 
-    public int send(String subject, String type, String text) {
-        return cMsgConstants.errorNotImplemented;
+    /**
+     * Method to close the connection to the domain server. This method results in this object
+     * becoming functionally useless.
+     *
+     * @throws cMsgException always throws an exception since this is a dummy implementation
+     */
+    public void disconnect() throws cMsgException {
+        throw new cMsgException("disconnect is not implemented yet");
     }
 
 
 //-----------------------------------------------------------------------------
 
 
-    public int flush() {
-        return cMsgConstants.errorNotImplemented;
+    /**
+     * Method to send a message to the domain server for further distribution.
+     *
+     * @param message message
+     * @throws cMsgException always throws an exception since this is a dummy implementation
+     */
+    public void send(cMsgMessage message) throws cMsgException {
+        throw new cMsgException("send is not implemented yet");
     }
 
 
 //-----------------------------------------------------------------------------
 
 
-    public int get(String subject, String type, int timeout, cMsgMessage msg) {
-        return cMsgConstants.errorNotImplemented;
+    /**
+     * Method to force cMsg client to send pending communications with domain server.
+     *
+     * @throws cMsgException always throws an exception since this is a dummy implementation
+     */
+    public void flush() throws cMsgException {
+        throw new cMsgException("flush is not implemented yet");
     }
 
 
 //-----------------------------------------------------------------------------
 
 
-    public int subscribe(String subject, String type, cMsgCallback cb, Object userObj) {
-        return cMsgConstants.errorNotImplemented;
+    /**
+     * Method to get a message for a particular subject and type from the domain server.
+     *
+     * @param subject message subject
+     * @param type    message type
+     * @param timeout time in milliseconds to wait for a message
+     * @param msg     object used to hold retrieved message
+     * @throws cMsgException always throws an exception since this is a dummy implementation
+     */
+    public void get(String subject, String type, int timeout, cMsgMessage msg) throws cMsgException {
+        throw new cMsgException("get is not implemented yet");
     }
 
 
 //-----------------------------------------------------------------------------
 
 
-    public int subscribe(String subject, String type, cMsgCallback cb) {
-        return cMsgConstants.errorNotImplemented;
+    /**
+     * Method to subscribe to receive messages of a subject and type from the domain server.
+     *
+     * @param subject message subject
+     * @param type    message type
+     * @param cb      callback object whose single method is called upon receiving a message
+     *                of subject and type
+     * @param userObj any user-supplied object to be given to the callback method as an argument
+     * @throws cMsgException always throws an exception since this is a dummy implementation
+     */
+    public void subscribe(String subject, String type, cMsgCallback cb, Object userObj) throws cMsgException {
+        throw new cMsgException("subscribe is not implemented yet");
     }
 
 
 //-----------------------------------------------------------------------------
 
 
-    public int done() {
-        return cMsgConstants.errorNotImplemented;
+    /**
+     * Method to unsubscribe a previous subscription to receive messages of a subject and type
+     * from the domain server. Since many subscriptions may be made to the same subject and type
+     * values, but with different callbacks, the callback must be specified so the correct
+     * subscription can be removed.
+     *
+     * @param subject message subject
+     * @param type    message type
+     * @param cb      callback object whose single method is called upon receiving a message
+     *                of subject and type
+     * @throws cMsgException always throws an exception since this is a dummy implementation
+     */
+    public void unsubscribe(String subject, String type, cMsgCallback cb) throws cMsgException {
+        throw new cMsgException("unsubscribe is not implemented yet");
     }
 
 
 //-----------------------------------------------------------------------------
 
 
-    public int start() {
-	 receiveState=true;
-        return cMsgConstants.errorNotImplemented;
+    /**
+     * Method to start or activate the subscription callbacks.
+     */
+    public void start() {
+	    receiving = true;
     }
 
 
 //-----------------------------------------------------------------------------
 
 
-    public int stop() {
-        receiveState=false;
-        return cMsgConstants.errorNotImplemented;
+    /**
+     * Method to stop or deactivate the subscription callbacks.
+     */
+    public void stop() {
+        receiving = false;
     }
 
 
 //-----------------------------------------------------------------------------
+
+
+    /**
+     * Get the name of the domain connected to.
+     * @return domain name
+     */
     public String getDomain() {return(domain);}
-    public void setDomain(String s) {domain = s;}
 
 
 //-----------------------------------------------------------------------------
 
 
+    /**
+     * Get the name of the client.
+     * @return client's name
+     */
     public String getName() {return(name);}
-    public void setName(String s) {name = s;}
 
 
 //-----------------------------------------------------------------------------
 
 
+    /**
+     * Get the client's description.
+     * @return client's description
+     */
     public String getDescription() {return(description);}
-    public void setDescription(String s) {description = s;}
 
 
 //-----------------------------------------------------------------------------
 
 
+    /**
+     * Get the host the client is running on.
+     * @return client's host
+     */
     public String getHost() {return(host);}
 
 
 //-----------------------------------------------------------------------------
 
 
-    public int getSendSocket() {return(sendSocket);}
-
-
-//-----------------------------------------------------------------------------
-
-
-    public int getReceiveSocket() {return(receiveSocket);}
-
-
-//-----------------------------------------------------------------------------
-
-
-    public int getPendThread() {return(pendThread);}
-
-
-//-----------------------------------------------------------------------------
-
-
-    public boolean getReceiveState() {return(receiveState);}
+    /**
+     * Get boolean tells whether callbacks are activated or not. The
+     * start and stop methods activate and deactivate the callbacks.
+     * @return true if callbacks are activated, false if they are not
+     */
+    public boolean isReceiving() {return(receiving);}
 
 
 }
