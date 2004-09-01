6eb41fcb961a347e595e372a9586901a8d4fe0e4
Carl Timmer <timmer@jlab.org>
2004-09-01 16:58:20 +0000

    changed comments
    (Logical change 1.36)

--- trunk/cMsg/cMsg_CODA.h
+++ trunk/cMsg/cMsg_CODA.h
@@ -67,19 +67,19 @@ typedef struct cMsgDomain_CODA_t {
   int listenSocket;    /* file descriptor for socket this program listens on for TCP connections */
   int keepAliveSocket; /* file descriptor for socket to tell if server is still alive or not */
 
-  char *myHost;      /* this hostname */
-  char *sendHost;    /* host to send messages to */
-  char *serverHost;  /* host cMsg name server lives on */
+  char *myHost;       /* this hostname */
+  char *sendHost;     /* host to send messages to */
+  char *serverHost;   /* host cMsg name server lives on */
 
-  char *name;        /* name of user (this program) */
-  char *udl;         /* UDL of cMsg name server */
-  char *description; /* user description */
+  char *name;         /* name of user (this program) */
+  char *udl;          /* UDL of cMsg name server */
+  char *description;  /* user description */
   
   unsigned short sendPort;   /* port to send messages to */
   unsigned short serverPort; /* port cMsg name server listens on */
   unsigned short listenPort; /* port this program listens on for this domain's TCP connections */
   
-  pthread_t pendThread; /* listening thread */
+  pthread_t pendThread;      /* listening thread */
   pthread_t keepAliveThread; /* thread sending keep alives to server */
   
   pthread_mutex_t sendMutex;      /* mutex to ensure thread-safety of send socket */
