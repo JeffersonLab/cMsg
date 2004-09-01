c94cc09bf84daf169277fa482e0a9fd8b4e7a30b
Carl Timmer <timmer@jlab.org>
2004-09-01 17:18:45 +0000

    add new constructor and some methods
    (Logical change 1.37)

--- trunk/cMsg/org/jlab/coda/cMsg/coda/cMsgSubscription.java
+++ trunk/cMsg/org/jlab/coda/cMsg/coda/cMsgSubscription.java
@@ -5,7 +5,7 @@
  *    This software was developed under a United States Government license    *
  *    described in the NOTICE file included as part of this distribution.     *
  *                                                                            *
- *    C. Timmer, 7-Jul-2004, Jefferson Lab                                    *
+ *    C. Timmer, 11-Aug-2004, Jefferson Lab                                    *
  *                                                                            *
  *     Author: Carl Timmer                                                    *
  *             timmer@jlab.org                   Jefferson Lab, MS-6B         *
@@ -16,27 +16,84 @@
 
 package org.jlab.coda.cMsg.coda;
 
+import java.util.HashSet;
+
 /**
- * Created by IntelliJ IDEA.
- * User: timmer
- * Date: Aug 11, 2004
- * Time: 11:24:58 AM
- * To change this template use File | Settings | File Templates.
+ * Class to store a client's subscription to a particular message subject and type.
+ *
+ * @author Carl Timmer
+ * @version 1.0
  */
 public class cMsgSubscription {
+
     /** Subject subscribed to. */
     String subject;
+
     /** Type subscribed to. */
     String type;
+
     /**
      * Id which eliminates the need to parse subject and type
-     * strings upon client's receipt of a message.
+     * strings upon client's receipt of a message. Sometimes referred
+     * to as receiverSubscribeId.
      */
     int id;
 
-    cMsgSubscription(String subject, String type, int id) {
+    /** Set of all callback information objects (cMsgCallbackInfo). */
+    HashSet callbacks;
+
+
+    /**
+     * Constructor.
+     * @param subject subscription subject
+     * @param type subscription type
+     * @param id unique id referring to subject and type combination
+     */
+    public cMsgSubscription(String subject, String type, int id) {
         this.subject = subject;
         this.type = type;
         this.id = id;
+    }
+
+
+    /**
+     * Constructor.
+     * @param subject subscription subject
+     * @param type subscription type
+     * @param id unique id referring to subject and type combination
+     * @param info object containing callback to be added and its argument
+     */
+    public cMsgSubscription(String subject, String type, int id, cMsgCallbackInfo info) {
+        this.subject = subject;
+        this.type = type;
+        this.id = id;
+        callbacks.add(info);
+    }
+
+
+    /**
+     * Method to add a callback.
+     * @param cbInfo  object containing callback to be added and its argument
+     */
+    public void addCallback(cMsgCallbackInfo cbInfo) {
+        callbacks.add(cbInfo);
+    }
+
+
+    /**
+     * Method to remove a callback.
+     * @param cbInfo  object containing callback to be removed
+     */
+    public void removeCallback(cMsgCallbackInfo cbInfo) {
+        callbacks.remove(cbInfo);
+    }
+
+
+    /**
+     * Method to return the number of callbacks registered.
+     * @return number of callback registered
+     */
+    public int numberOfCallbacks() {
+        return callbacks.size();
     }
 }
