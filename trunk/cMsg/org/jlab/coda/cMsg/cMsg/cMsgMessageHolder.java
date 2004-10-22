d2530fd62ee7cc106915a515e778ab5a04d32a35
Carl Timmer <timmer@jlab.org>
2004-10-22 18:36:00 +0000

    (Logical change 1.73)

---
+++ trunk/cMsg/org/jlab/coda/cMsg/cMsg/cMsgMessageHolder.java
@@ -0,0 +1,29 @@
+/*----------------------------------------------------------------------------*
+ *  Copyright (c) 2004        Southeastern Universities Research Association, *
+ *                            Thomas Jefferson National Accelerator Facility  *
+ *                                                                            *
+ *    This software was developed under a United States Government license    *
+ *    described in the NOTICE file included as part of this distribution.     *
+ *                                                                            *
+ *    C. Timmer, 22-Oct-2004, Jefferson Lab                                    *
+ *                                                                            *
+ *     Author: Carl Timmer                                                    *
+ *             timmer@jlab.org                   Jefferson Lab, MS-6B         *
+ *             Phone: (757) 269-5130             12000 Jefferson Ave.         *
+ *             Fax:   (757) 269-5800             Newport News, VA 23606       *
+ *                                                                            *
+ *----------------------------------------------------------------------------*/
+
+package org.jlab.coda.cMsg.cMsg;
+
+import org.jlab.coda.cMsg.cMsgMessage;
+
+/**
+ * Class used to help in implementing a client's "get" method. An object of this
+ * class stores a msg from the server to the "get" caller and is used to
+ * synchronize/wait/notify on.
+ */
+class cMsgMessageHolder {
+    /** Location to store message object. */
+    cMsgMessage message;
+}
