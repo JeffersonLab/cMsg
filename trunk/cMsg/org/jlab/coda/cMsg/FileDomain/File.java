7053f207ffdf9739e10b57f64866f030f40fd91f
Elliot Wolin <wolin@jlab.org>
2004-11-16 15:47:00 +0000

    Doc
    2004/11/16 10:42:49-05:00 jlab.org!wolin
    Added textOnly
    
    2004/11/16 09:55:37-05:00 jlab.org!wolin
    What to write...text, message?
    
    (Logical change 1.93)

--- trunk/cMsg/org/jlab/coda/cMsg/FileDomain/File.java
+++ trunk/cMsg/org/jlab/coda/cMsg/FileDomain/File.java
@@ -1,5 +1,4 @@
 // still to do:
-//   what to log?  message?  text only?
 
 
 /*----------------------------------------------------------------------------*
@@ -26,6 +25,7 @@ import java.io.*;
 import java.util.*;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
+import java.net.*;
 
 
 //-----------------------------------------------------------------------------
@@ -40,9 +40,9 @@ import java.util.regex.Pattern;
  */
 public class File extends cMsgAdapter {
 
-    String myFileName;
-    PrintWriter myPrintHandle;
-
+    private String myFileName;
+    private PrintWriter myPrintHandle;
+    private boolean textOnly = false;
 
 
 //-----------------------------------------------------------------------------
@@ -51,6 +51,9 @@ public class File extends cMsgAdapter {
     /**
      * Constructor for File domain.
      *
+     * Default is to print entire message to file.
+     * set textOnly=true in UDL to only print timestamp and message text.
+     *
      * @param UDL Uniform Domain Locator holds file name
      * @param name does not need to be unique
      * @param description description of this client
@@ -58,30 +61,44 @@ public class File extends cMsgAdapter {
      */
     public File(String UDL, String name, String description) throws cMsgException {
 
+	Pattern p;
+	Matcher m;
+	String remainder = null;
+
 	// save params
         this.UDL         = UDL;
         this.name        = name;
         this.description = description;
 
+	try {
+	    this.host = InetAddress.getLocalHost().getHostName();
+	} catch (UnknownHostException e) {
+	    System.err.println(e);
+	    this.host="unknown";
+	}
+
 
 	// parse file name
-        Pattern p = Pattern.compile("^\\s*(cMsg:)?File://(.+)$",Pattern.CASE_INSENSITIVE);
-        Matcher m;
+	p = Pattern.compile("^\\s*(cMsg:)?File://(.+)$",Pattern.CASE_INSENSITIVE);
 	int ind = UDL.indexOf('?');
 	if(ind<=0) {
-	    m = p.matcher(UDL);
+	    domain = UDL;
 	} else {
-	    m = p.matcher(UDL.substring(0,ind));
+	    domain    = UDL.substring(0,ind);
+	    remainder = UDL.substring(ind+1);
 	}
-        try {
+	m = p.matcher(domain);
+	m.find();
+	myFileName = m.group(2);
+	
+
+	// parse remainder
+	if(remainder!=null) {
+	    p = Pattern.compile("textOnly=(\\w+)",Pattern.CASE_INSENSITIVE);
+	    m = p.matcher(remainder);
 	    m.find();
-	    myFileName = m.group(2);
-        } catch (Exception e) {
-            e.printStackTrace();
-            cMsgException ce = new cMsgException(e.getMessage());
-            ce.setReturnCode(1);
-            throw ce;
-        }
+	    textOnly = m.group(1).equals("true");
+	}
     }
 
 
@@ -96,9 +113,8 @@ public class File extends cMsgAdapter {
     public void connect() throws cMsgException {
 
 	try{
-	    PrintWriter pw = new PrintWriter(new BufferedWriter(new FileWriter(myFileName, true)));
-	    myPrintHandle = pw;
-	    pw.println("<cMsgFile  name=\"" + myFileName + "\"" + "  date=\"" + (new Date()) + "\">\n\n");
+	    myPrintHandle = new PrintWriter(new BufferedWriter(new FileWriter(myFileName, true)));
+	    myPrintHandle.println("<cMsgFile  name=\"" + myFileName + "\"" + "  date=\"" + (new Date()) + "\">\n\n");
 	} catch (IOException e) {
 	    System.out.println(e);
 	    e.printStackTrace();
@@ -128,14 +144,25 @@ public class File extends cMsgAdapter {
 
 
     /**
-     * Writes to file
+     * Writes to file.
      *
      * @param message message to send
      * @throws cMsgException (not thrown)
      */
-    public void send(cMsgMessage message) throws cMsgException {
-	//	myPrintHandle.println(message);
-	myPrintHandle.println(message.getText());  // ???
+    public void send(cMsgMessage msg) throws cMsgException {
+	Date now = new Date();
+	if(textOnly) {
+	    myPrintHandle.println(now + ":    " + msg.getText());
+	} else {
+	    msg.setDomain(domain);
+	    msg.setSender(name);
+	    msg.setSenderHost(host);
+	    msg.setSenderTime(now);
+	    msg.setReceiver(domain);
+	    msg.setReceiverTime(now);
+	    msg.setReceiverHost(host);
+	    myPrintHandle.println(msg);
+	}
     }
 
 
@@ -162,6 +189,7 @@ public class File extends cMsgAdapter {
      * Flushes output.
      */
     public void flush() {
+	myPrintHandle.flush();
         return;
     }
 
