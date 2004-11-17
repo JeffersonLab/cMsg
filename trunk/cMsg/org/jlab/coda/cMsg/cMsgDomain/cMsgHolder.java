316989e056f235b29055ba4b500d33fa6a9ff223
Carl Timmer <timmer@jlab.org>
2004-11-17 20:17:38 +0000

    change thread pool to wait on blocking linked queue, increase speed
    (Logical change 1.96)

--- trunk/cMsg/org/jlab/coda/cMsg/cMsgDomain/cMsgHolder.java
+++ trunk/cMsg/org/jlab/coda/cMsg/cMsgDomain/cMsgHolder.java
@@ -17,13 +17,50 @@
 package org.jlab.coda.cMsg.cMsgDomain;
 
 import org.jlab.coda.cMsg.cMsgMessage;
+import java.nio.channels.SocketChannel;
 
 /**
  * Class used to help in implementing a client's "get" method. An object of this
  * class stores a msg from the server to the "get" caller and is used to
  * synchronize/wait/notify on.
+ *
+ * Also used to implement Domain server by storing an incoming message along with
+ * subject, type, and id for later action by a thread from the thread pool.
  */
-class cMsgMessageHolder {
+class cMsgHolder {
     /** Location to store message object. */
     cMsgMessage message;
+
+    /** Store subject. */
+    String subject;
+
+    /** Store type. */
+    String type;
+
+    /** Store id. */
+    int id;
+
+    /** Store request. */
+    int request;
+
+    /** Store communication channel. */
+    SocketChannel channel;
+
+    public cMsgHolder() {
+    }
+
+    public cMsgHolder(cMsgMessage message) {
+        this.message = message;
+    }
+    public cMsgHolder(cMsgMessage message, int request) {
+        this.message = message;
+        this.request = request;
+    }
+
+    public cMsgHolder(String subject, String type, int id, int request) {
+        this.request = request;
+        this.subject = subject;
+        this.type = type;
+        this.id = id;
+    }
 }
