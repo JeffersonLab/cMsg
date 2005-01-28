6242f302ac476bca91213ceee2c3657d9f46ed72
Elliot Wolin <wolin@jlab.org>
2005-01-28 18:30:07 +0000

    Removed unneeded functions
    (Logical change 1.204)

--- trunk/cMsg/org/jlab/coda/cMsg/subdomains/LogFile.java
+++ trunk/cMsg/org/jlab/coda/cMsg/subdomains/LogFile.java
@@ -1,6 +1,4 @@
 // still to do:
-//   server shutdown?
-
 
 
 /*----------------------------------------------------------------------------*
@@ -25,7 +23,7 @@ package org.jlab.coda.cMsg.subdomains;
 
 import org.jlab.coda.cMsg.cMsgMessageFull;
 import org.jlab.coda.cMsg.cMsgException;
-import org.jlab.coda.cMsg.cMsgSubdomainInterface;
+import org.jlab.coda.cMsg.cMsgSubdomainAdapter;
 import org.jlab.coda.cMsg.cMsgClientInfo;
 
 import java.util.*;
@@ -46,7 +44,7 @@ import java.util.regex.*;
  * @author Elliott Wolin
  * @version 1.0
  */
-public class LogFile implements cMsgSubdomainInterface {
+public class LogFile extends cMsgSubdomainAdapter {
 
 
     /** Hash table to store all client info.  Canonical name is key. */
@@ -55,8 +53,8 @@ public class LogFile implements cMsgSubdomainInterface {
 
     /** Inner class to hold log file information. */
     private static class LogFileObject {
-	PrintWriter printHandle;
-	AtomicInteger count;
+        PrintWriter printHandle;
+        AtomicInteger count;
 
         LogFileObject(PrintWriter handle) {
             printHandle = handle;
@@ -117,66 +115,6 @@ public class LogFile implements cMsgSubdomainInterface {
 
 
     /**
-     * Method to tell if the "subscribeAndGet" cMsg API function is implemented
-     * by this interface implementation in the {@link #handleSubscribeAndGetRequest}
-     * method.
-     *
-     * @return true if subscribeAndGet implemented in {@link #handleSubscribeAndGetRequest}
-     */
-    public boolean hasSubscribeAndGet() {
-        return false;
-    }
-
-
-//-----------------------------------------------------------------------------
-
-
-    /**
-     * Method to tell if the "sendAndGet" cMsg API function is implemented
-     * by this interface implementation in the {@link #handleSendAndGetRequest}
-     * method.
-     *
-     * @return true if sendAndGet implemented in {@link #handleSendAndGetRequest}
-     */
-    public boolean hasSendAndGet() {
-        return false;
-    }
-
-
-//-----------------------------------------------------------------------------
-
-
-    /**
-     * Method to tell if the "subscribe" cMsg API function is implemented
-     * by this interface implementation in the {@link #handleSubscribeRequest}
-     * method.
-     *
-     * @return true if subscribe implemented in {@link #handleSubscribeRequest}
-     */
-    public boolean hasSubscribe() {
-        return false;
-    };
-
-
-//-----------------------------------------------------------------------------
-
-
-    /**
-     * Method to tell if the "unsubscribe" cMsg API function is implemented
-     * by this interface implementation in the {@link #handleUnsubscribeRequest}
-     * method.
-     *
-     * @return true if unsubscribe implemented in {@link #handleUnsubscribeRequest}
-     */
-    public boolean hasUnsubscribe() {
-        return false;
-    };
-
-
-//-----------------------------------------------------------------------------
-
-
-    /**
      * Method to give the subdomain handler the appropriate part
      * of the UDL the client used to talk to the domain server.
      *
@@ -204,16 +142,16 @@ public class LogFile implements cMsgSubdomainInterface {
 
 
         //  extract file name from UDL remainder
-	if (myUDLRemainder.indexOf("?") > 0) {
-	    Pattern p = Pattern.compile("^(.+?)(\\?)(.*)$");
-	    Matcher m = p.matcher(myUDLRemainder);
-	    m.find();
-	    myFileName = m.group(1);
-	    remainder = m.group(2);
-	}
-	else {
-	    myFileName = myUDLRemainder;
-	}
+        if (myUDLRemainder.indexOf("?") > 0) {
+            Pattern p = Pattern.compile("^(.+?)(\\?)(.*)$");
+            Matcher m = p.matcher(myUDLRemainder);
+            m.find();
+            myFileName = m.group(1);
+            remainder = m.group(2);
+        }
+        else {
+            myFileName = myUDLRemainder;
+        }
 
 
         // get canonical name
@@ -267,7 +205,7 @@ public class LogFile implements cMsgSubdomainInterface {
      */
     public void handleSendRequest(cMsgMessageFull msg) throws cMsgException {
         msg.setReceiver("cMsg:LogFile");
-	myPrintHandle.println(msg);
+        myPrintHandle.println(msg);
     }
 
 
@@ -285,112 +223,6 @@ public class LogFile implements cMsgSubdomainInterface {
     public int handleSyncSendRequest(cMsgMessageFull msg) throws cMsgException {
         handleSendRequest(msg);
         return 0;
-    }
-
-
-//-----------------------------------------------------------------------------
-
-
-    /**
-     * Method to synchronously get a single message from a receiver by sending out a
-     * message to be responded to.
-     *
-     * @param message message requesting what sort of message to get
-     */
-    public void handleSendAndGetRequest(cMsgMessageFull message) {
-        // do nothing
-    }
-
-
-//-----------------------------------------------------------------------------
-
-
-    /**
-     * Method to synchronously get a single message from the server for a one-time
-     * subscription of a subject and type.
-     *
-     * @param subject message subject subscribed to
-     * @param type    message type subscribed to
-     * @param id      message id refering to these specific subject and type values
-     */
-    public void handleSubscribeAndGetRequest(String subject, String type, int id) {
-        // no nothing
-    }
-
-
-//-----------------------------------------------------------------------------
-
-
-    /**
-     * Method to handle remove sendAndGet request sent by domain client
-     * (hidden from user).
-     *
-     * @param id message id refering to these specific subject and type values
-     */
-    public void handleUnSendAndGetRequest(int id) {
-        // do nothing
-    }
-
-
-//-----------------------------------------------------------------------------
-
-
-    /**
-     * Method to handle remove subscribeAndGet request sent by domain client
-     * (hidden from user).
-     *
-     * @param id message id refering to these specific subject and type values
-     */
-    public void handleUnSubscribeAndGetRequest(int id) {
-        // do nothing
-    }
-
-
-//-----------------------------------------------------------------------------
-
-
-    /**
-     * Method to handle subscribe request sent by domain client.
-     *
-     * @param subject message subject to subscribe to
-     * @param type message type to subscribe to
-     * @param receiverSubscribeId message id refering to these specific subject and type values
-     * @throws cMsgException if no client information is available or a subscription for this
-     *                          subject and type already exists
-     */
-    public void handleSubscribeRequest(String subject, String type,
-                                       int receiverSubscribeId) throws cMsgException {
-        // do nothing...
-    }
-
-
-//-----------------------------------------------------------------------------
-
-
-    /**
-     * Method to handle sunsubscribe request sent by domain client.
-     * This method is run after all exchanges between domain server and client.
-     *
-     * @param subject message subject subscribed to
-     * @param type message type subscribed to
-     * @param receiverSubscribeId message id refering to these specific subject and type values
-     */
-    public void handleUnsubscribeRequest(String subject, String type, int receiverSubscribeId) {
-        // do nothing...
-    }
-
-
-//-----------------------------------------------------------------------------
-
-
-    /**
-     * Method to handle keepalive sent by domain client checking to see
-     * if the domain server socket is still up. Normally nothing needs to
-     * be done as the domain server simply returns an "OK" to all keepalives.
-     * This method is run after all exchanges between domain server and client.
-     */
-    public void handleKeepAlive() {
-        // do nothing...
     }
 
 
@@ -416,20 +248,5 @@ public class LogFile implements cMsgSubdomainInterface {
 
 
 //-----------------------------------------------------------------------------
-
-
-    /**
-     * Method to handle a complete name server shutdown.
-     * This method is run after all exchanges between domain server and client but
-     * before the server is killed (since that is what is running this
-     * method).
-     */
-    public void handleServerShutdown() throws cMsgException {
-	    // not working yet...
-    }
-
-}
-
-
 //-----------------------------------------------------------------------------
-
+}
