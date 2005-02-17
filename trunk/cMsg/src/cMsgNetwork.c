3f3c25c902b383413f6ab9f4d658787a15228f29
Carl Timmer <timmer@jlab.org>
2005-02-17 18:27:42 +0000

    move vxWorks conflicting header
    (Logical change 1.230)

--- trunk/cMsg/src/cMsgNetwork.c
+++ trunk/cMsg/src/cMsgNetwork.c
@@ -36,13 +36,13 @@
 #include <sys/time.h>
 #include <sys/select.h>
 #include <sys/utsname.h>
+#include <strings.h>
 
 #endif
 
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include <strings.h>
 #include <unistd.h>
 #include <errno.h>
 #include <fcntl.h>
