54d524c5e8d38ceb2eb168e417e6b75120ec0c3e
Elliot Wolin <wolin@jlab.org>
2005-04-13 16:44:27 +0000

    Added const, may need more
    (Logical change 1.271)

--- trunk/cMsg/src/cMsgBase.hxx
+++ trunk/cMsg/src/cMsgBase.hxx
@@ -55,8 +55,8 @@ public:
   ~cMsgException(void);
 
   virtual void setReturnCode(int i);
-  virtual int getReturnCode(void);
-  virtual string toString(void);
+  virtual int getReturnCode(void) const;
+  virtual string toString(void) const;
 };
 
 
@@ -93,38 +93,38 @@ public:
   cMsgMessageBase(void *msgPointer) throw(cMsgException);
   ~cMsgMessageBase(void) throw(cMsgException);
 
-  virtual string getSubject(void) throw(cMsgException);
+  virtual string getSubject(void) const throw(cMsgException);
   virtual void setSubject(const string &subject) throw(cMsgException);
-  virtual string getType(void) throw(cMsgException);
+  virtual string getType(void) const throw(cMsgException);
   virtual void setType(const string &type) throw(cMsgException);
-  virtual string getText(void) throw(cMsgException);
+  virtual string getText(void) const throw(cMsgException);
   virtual void setText(const string &text) throw(cMsgException);
-  virtual int getUserInt(void) throw(cMsgException);
+  virtual int getUserInt(void) const throw(cMsgException);
   virtual void setUserInt(int i) throw(cMsgException);
-  virtual timespec getUserTime(void) throw(cMsgException);
+  virtual timespec getUserTime(void) const throw(cMsgException);
   virtual void setUserTime(const timespec &userTime) throw(cMsgException);
-  virtual int getVersion(void) throw(cMsgException);
-  virtual string getDomain(void) throw(cMsgException);
-  virtual string getCreator(void) throw(cMsgException);
-  virtual string getReceiver(void) throw(cMsgException);
-  virtual string getReceiverHost(void) throw(cMsgException);
-  virtual string getSender(void) throw(cMsgException);
-  virtual string getSenderHost(void) throw(cMsgException);
-  virtual timespec getReceiverTime(void) throw(cMsgException);
-  virtual timespec getSenderTime(void) throw(cMsgException);
-  virtual bool isGetRequest(void) throw(cMsgException);
-  virtual bool isGetResponse(void) throw(cMsgException);
-  virtual bool isNullGetResponse(void) throw(cMsgException);
+  virtual int getVersion(void) const throw(cMsgException);
+  virtual string getDomain(void) const throw(cMsgException);
+  virtual string getCreator(void) const throw(cMsgException);
+  virtual string getReceiver(void) const throw(cMsgException);
+  virtual string getReceiverHost(void) const throw(cMsgException);
+  virtual string getSender(void) const throw(cMsgException);
+  virtual string getSenderHost(void) const throw(cMsgException);
+  virtual timespec getReceiverTime(void) const throw(cMsgException);
+  virtual timespec getSenderTime(void) const throw(cMsgException);
+  virtual bool isGetRequest(void) const throw(cMsgException);
+  virtual bool isGetResponse(void) const throw(cMsgException);
+  virtual bool isNullGetResponse(void) const throw(cMsgException);
   virtual void makeNullResponse(cMsgMessageBase &msg) throw(cMsgException);
   virtual void makeNullResponse(cMsgMessageBase *msg) throw(cMsgException);
   virtual void makeResponse(cMsgMessageBase &msg) throw(cMsgException);
   virtual void makeResponse(cMsgMessageBase *msg) throw(cMsgException);
   virtual void setGetResponse(bool b) throw(cMsgException);
   virtual void setNullGetResponse(bool b) throw(cMsgException);
-  virtual string toString(void) throw(cMsgException);
-  virtual cMsgMessageBase copy(void) throw(cMsgException);
-  virtual cMsgMessageBase nullResponse(void) throw(cMsgException);
-  virtual cMsgMessageBase response(void) throw(cMsgException);
+  virtual string toString(void) const throw(cMsgException);
+  virtual cMsgMessageBase copy(void) const throw(cMsgException);
+  virtual cMsgMessageBase nullResponse(void) const throw(cMsgException);
+  virtual cMsgMessageBase response(void) const throw(cMsgException);
 };
 
 
@@ -276,11 +276,11 @@ public:
   virtual void flush(void) throw(cMsgException);
   virtual void start(void) throw(cMsgException);
   virtual void stop(void) throw(cMsgException);
-  virtual string getUDL(void);
-  virtual string getName(void);
-  virtual string getDescription(void);
-  virtual bool isConnected(void);
-  virtual bool isReceiving(void);
+  virtual string getUDL(void) const;
+  virtual string getName(void) const;
+  virtual string getDescription(void) const;
+  virtual bool isConnected(void) const;
+  virtual bool isReceiving(void) const;
   virtual void setShutdownHandler(cMsgShutdownHandlerInterface *handler, void* userArg) throw(cMsgException);
   virtual void shutdown(string &client, string &server, int flag) throw(cMsgException);
 };
