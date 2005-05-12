a3f1b4411268189d12505eb578d7c06dbbe76e3e
Carl Timmer <timmer@jlab.org>
2005-05-12 20:25:34 +0000

    change IO to streams since nio can't handle large buffer sizes well
    (Logical change 1.305)

--- trunk/cMsg/org/jlab/coda/cMsg/cMsgDomain/server/cMsgMessageDeliverer.java
+++ trunk/cMsg/org/jlab/coda/cMsg/cMsgDomain/server/cMsgMessageDeliverer.java
@@ -19,8 +19,7 @@ package org.jlab.coda.cMsg.cMsgDomain.server;
 import org.jlab.coda.cMsg.*;
 import org.jlab.coda.cMsg.cMsgDomain.cMsgUtilities;
 
-import java.io.IOException;
-import java.io.UnsupportedEncodingException;
+import java.io.*;
 import java.nio.channels.SocketChannel;
 import java.nio.ByteBuffer;
 import java.net.InetSocketAddress;
@@ -82,18 +81,20 @@ public class cMsgMessageDeliverer implements cMsgDeliverMessageInterface {
       * @param info client information object
       * @throws IOException if socket cannot be created
       */
-    public static void createChannel(cMsgClientInfo info) throws IOException {
-         SocketChannel channel = SocketChannel.open(new InetSocketAddress(info.getClientHost(),
-                                                            info.getClientPort()));
-         // set socket options
-         Socket socket = channel.socket();
-         // Set tcpNoDelay so no packets are delayed
-         socket.setTcpNoDelay(true);
-         // set buffer sizes
-         socket.setReceiveBufferSize(65535);
-         socket.setSendBufferSize(65535);
-         info.setChannel(channel);
-     }
+    public void createChannel(cMsgClientInfo info) throws IOException {
+        SocketChannel channel = SocketChannel.open(new InetSocketAddress(info.getClientHost(),
+                                                                         info.getClientPort()));
+        // set socket options
+        Socket socket = channel.socket();
+        // Set tcpNoDelay so no packets are delayed
+        socket.setTcpNoDelay(true);
+        // set buffer sizes
+        socket.setReceiveBufferSize(65535);
+        socket.setSendBufferSize(65535);
+
+        // store connection in info object
+        info.setChannel(channel);
+    }
 
 
     /**
@@ -104,7 +105,7 @@ public class cMsgMessageDeliverer implements cMsgDeliverMessageInterface {
      * @return SocketChannel object for communicating with client
      * @throws IOException if socket cannot be created
      */
-    public static SocketChannel createChannel(String host, int port) throws IOException {
+    public SocketChannel createChannel(String host, int port) throws IOException {
         SocketChannel channel = SocketChannel.open(new InetSocketAddress(host, port));
         // set socket options
         Socket socket = channel.socket();
@@ -129,6 +130,122 @@ public class cMsgMessageDeliverer implements cMsgDeliverMessageInterface {
      *                             or client returns an error
      */
     private boolean deliverMessageReal(cMsgMessageFull msg, cMsgClientInfo info,
+                                       int msgType, boolean acknowledge) throws IOException {
+
+        DataInputStream in = info.getInputStream();
+        DataOutputStream out = info.getOutputStream();
+
+        if (in == null || out == null) {
+            createChannel(info);
+            in  = info.getInputStream();
+            out = info.getOutputStream();
+        }
+
+        // if a get has a null response ...
+        boolean nullResponse = false;
+        if (msgType == cMsgConstants.msgGetResponseIsNull) {
+            nullResponse = true;
+        }
+
+        if (msgType == cMsgConstants.msgShutdown) {
+            // size
+            out.writeInt(8);
+            // msg type
+            out.writeInt(msgType);
+            // want an acknowledgment?
+            out.writeInt(acknowledge ? 1 : 0);
+        }
+        else if (nullResponse) {
+            // size
+            out.writeInt(12);
+            // msg type
+            out.writeInt(msgType);
+            // senderToken
+            out.writeInt(msg.getSenderToken());
+            // want an acknowledgment?
+            out.writeInt(acknowledge ? 1 : 0);
+        }
+        else {
+            // write 20 ints
+
+            int len1 = msg.getSender().length();
+            int len2 = msg.getSenderHost().length();
+            int len3 = msg.getSubject().length();
+            int len4 = msg.getType().length();
+            int len5 = msg.getCreator().length();
+            int len6 = msg.getText().length();
+            int binLength = msg.getByteArrayLength();
+            // size of everything sent (except "size" itself which is first integer)
+            int size = len1 + len2 + len3 + len4 + len5 + len6 +
+                       binLength + 4*(19);
+
+            out.writeInt(size);
+            out.writeInt(msgType);
+            out.writeInt(msg.getVersion());
+            out.writeInt(0); // reserved for future use
+            out.writeInt(msg.getUserInt());
+            out.writeInt(msg.getInfo());
+
+            // send the time in milliseconds as 2, 32 bit integers
+            out.writeInt((int) (msg.getSenderTime().getTime() >>> 32)); // higher 32 bits
+            out.writeInt((int) (msg.getSenderTime().getTime() & 0x00000000FFFFFFFFL)); // lower 32 bits
+            out.writeInt((int) (msg.getUserTime().getTime() >>> 32));
+            out.writeInt((int) (msg.getUserTime().getTime() & 0x00000000FFFFFFFFL));
+
+            out.writeInt(msg.getSysMsgId());
+            out.writeInt(msg.getSenderToken());
+            out.writeInt(len1);
+            out.writeInt(len2);
+            out.writeInt(len3);
+            out.writeInt(len4);
+            out.writeInt(len5);
+            out.writeInt(len6);
+            out.writeInt(binLength);
+            out.writeInt(acknowledge ? 1 : 0);
+
+            // write strings
+            try {
+                out.write(msg.getSender().getBytes("US-ASCII"));
+                out.write(msg.getSenderHost().getBytes("US-ASCII"));
+                out.write(msg.getSubject().getBytes("US-ASCII"));
+                out.write(msg.getType().getBytes("US-ASCII"));
+                out.write(msg.getCreator().getBytes("US-ASCII"));
+                out.write(msg.getText().getBytes("US-ASCII"));
+                if (binLength > 0) {
+                    out.write(msg.getByteArray(),
+                               msg.getByteArrayOffset(),
+                               binLength);
+                }
+            }
+            catch (UnsupportedEncodingException e) {
+                e.printStackTrace();
+            }
+            out.flush();
+
+        }
+
+        // If no acknowledgment required, return
+        if (!acknowledge) {
+            return true;
+        }
+
+        if (in.readInt() != cMsgConstants.ok) return false;
+        return true;
+    }
+
+
+    /**
+     * Method to deliver a message to a client.
+     *
+     * @param msg     message to be sent
+     * @param info information about the client
+     * @param msgType type of communication with the client
+     * @param acknowledge if acknowledgement of message is desired, set to true
+     * @return true if message acknowledged by receiver or acknowledgment undesired, otherwise false
+     * @throws java.io.IOException if the message cannot be sent over the channel
+     *                             or client returns an error
+     */
+    private boolean deliverMessageRealNIO(cMsgMessageFull msg, cMsgClientInfo info,
                                        int msgType, boolean acknowledge) throws IOException {
 
         int binaryLength = 0;
@@ -245,5 +362,6 @@ public class cMsgMessageDeliverer implements cMsgDeliverMessageInterface {
         if (buffer.getInt() != cMsgConstants.ok) return false;
         return true;
     }
+
 
 }
