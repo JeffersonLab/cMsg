27de04b06717d990b13475a5dd3bfe2a52a0d137
Carl Timmer <timmer@jlab.org>
2006-01-27 15:40:22 +0000

    cosmetic

--- trunk/cMsg/org/jlab/coda/cMsg/apps/cMsgConsumer.java
+++ trunk/cMsg/org/jlab/coda/cMsg/apps/cMsgConsumer.java
@@ -1,3 +1,19 @@
+/*----------------------------------------------------------------------------*
+ *  Copyright (c) 2004        Southeastern Universities Research Association, *
+ *                            Thomas Jefferson National Accelerator Facility  *
+ *                                                                            *
+ *    This software was developed under a United States Government license    *
+ *    described in the NOTICE file included as part of this distribution.     *
+ *                                                                            *
+ *    C. Timmer,       2004, Jefferson Lab                                    *
+ *                                                                            *
+ *     Author: Carl Timmer                                                    *
+ *             timmer@jlab.org                   Jefferson Lab, MS-6B         *
+ *             Phone: (757) 269-5130             12000 Jefferson Ave.         *
+ *             Fax:   (757) 269-5800             Newport News, VA 23606       *
+ *                                                                            *
+ *----------------------------------------------------------------------------*/
+
 package org.jlab.coda.cMsg.apps;
 
 import org.jlab.coda.cMsg.*;
@@ -9,7 +25,7 @@ public class cMsgConsumer {
 
     String  name = "consumer";
     String  description = "java consumer";
-    String  UDL = "cMsg:cMsg://phecda:3456/cMsg/test";
+    String  UDL = "cMsg:cMsg://aslan:3456/cMsg/test";
     String  subject = "SUBJECT";
     String  type = "TYPE";
     boolean debug;
