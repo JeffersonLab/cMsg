a7a13fa7f0ef473f22bc4203aa07d219009fb907
Carl Timmer <timmer@jlab.org>
2007-03-19 18:45:51 +0000

    make multithreaded

--- trunk/cMsg/org/jlab/coda/cMsg/apps/RCBroadcastServer.java
+++ trunk/cMsg/org/jlab/coda/cMsg/apps/RCBroadcastServer.java
@@ -18,62 +18,50 @@ package org.jlab.coda.cMsg.apps;
 
 import org.jlab.coda.cMsg.*;
 
-import java.util.concurrent.CountDownLatch;
-
 /**
  * Simulates an rc broadcast server and rc server together. It dies and them comes back.
  */
 public class RCBroadcastServer {
 
-    String rcClientHost;
-    int rcClientTcpPort;
-    String name;
-    CountDownLatch latch;
-    int count;
-    cMsg cmsg;
-
     class BroadcastCallback extends cMsgCallbackAdapter {
          public void callback(cMsgMessage msg, Object userObject) {
-             rcClientHost = msg.getSenderHost();
-             rcClientTcpPort = msg.getUserInt();
-             name = msg.getSender();
+             String host = msg.getSenderHost();
+             int    port = msg.getUserInt();
+             String name = msg.getSender();
              
-             System.out.println("RAN CALLBACK, host = " + rcClientHost +
-                                ", port = " + rcClientTcpPort +
-                                ", name = " + name);
+             //System.out.println("Ran broadcast cb, host = " + host +
+             //                   ", port = " + port +
+             //                   ", name = " + name);
 
-             // abort connection
-             /*
-             try {
-                 cmsg.send(msg);
-             }
-             catch (cMsgException e) { }
-             */
+             // now that we have the message, we know what TCP host and port
+             // to connect to in the RC Server domain.
+             //System.out.println("Starting RC Server");
 
-             // finish connection
-             latch.countDown();
+             //RcServerReconnectThread rcserver  = new RcServerReconnectThread(host, port);
+             ConnectDisconnectThread rcserver2 = new ConnectDisconnectThread(host, port);
+             rcserver2.start();
         }
     }
 
 
     class rcCallback extends cMsgCallbackAdapter {
         public void callback(cMsgMessage msg, Object userObject) {
-            count++;
             String s = (String) userObject;
-            System.out.println("RAN CALLBACK, msg text = " + msg.getText() + ", for " + s);
+            System.out.println("RAN CALLBACK, msg sub = " + msg.getSubject() +
+                    ", type " + msg.getType() + ", text = " + msg.getText() + ", for " + s);
         }
     }
 
     class nullCallback extends cMsgCallbackAdapter {
         public void callback(cMsgMessage msg, Object userObject) {
-            count++;
-            System.out.println("RAN CALLBACK, for NULL subscription");
+            String s = (String) userObject;
+            System.out.println("RAN CALLBACK, for NULL subscription, msg sub = " + msg.getSubject() +
+                    ", type " + msg.getType() + ", text = " + msg.getText() + ", for " + s);
         }
     }
 
 
     public RCBroadcastServer() {
-        latch = new CountDownLatch(1);
     }
 
 
@@ -99,7 +87,7 @@ public class RCBroadcastServer {
 
         String UDL = "cMsg:rcb://?expid=carlExp";
 
-        cmsg = new cMsg(UDL, "broadcast listener", "udp trial");
+        cMsg cmsg = new cMsg(UDL, "broadcast listener", "udp trial");
         try {cmsg.connect();}
         catch (cMsgException e) {
             System.out.println(e.getMessage());
@@ -113,77 +101,121 @@ public class RCBroadcastServer {
         // subject and type are ignored in this domain
         cmsg.subscribe("sub", "type", cb, null);
 
-        // wait for incoming message from rc client
-        try { latch.await(); }
-        catch (InterruptedException e) {}
+    }
 
+    /**
+     * Class for handling client which does endless connects and disconnects.
+     */
+    class ConnectDisconnectThread extends Thread{
 
+        String rcClientHost;
+        int rcClientTcpPort;
 
-        // now that we have the message, we know what TCP host and port
-        // to connect to in the RC Server domain.
-        System.out.println("Starting RC Server");
-
-        // send stuff to rc client
-        cMsgMessage msg = new cMsgMessage();
-        msg.setSubject("rcSubject");
-        msg.setType("rcType");
-
-        //try {Thread.sleep(4000);}
-        //catch (InterruptedException e) {}
-
-        // RC Server domain UDL is of the form:
-        //       cMsg:rcs://<host>:<tcpPort>?port=<udpPort>
-        //
-        // The intial cMsg:rcs:// is stripped off by the top layer API
-        //
-        // Remember that for this domain:
-        // 1) host is NOT optional (must start with an alphabetic character according to "man hosts" or
-        //    may be in dotted form (129.57.35.21)
-        // 2) host can be "localhost"
-        // 3) tcp port is optional and defaults to 7654 (cMsgNetworkConstants.rcClientPort)
-        // 4) the udp port to listen on may be given by the optional port parameter.
-        //    if it's not given, the system assigns one
-        String rcsUDL = "cMsg:rcs://" + rcClientHost + ":" + rcClientTcpPort + "/?port=5859";
-
-        cMsg server1 = new cMsg(rcsUDL, "rc server", "udp trial");
-        server1.connect();
-
-        server1.start();
-
-        rcCallback cb2 = new rcCallback();
-        Object unsub = server1.subscribe("subby", "typey", cb2, "1st sub");
-
-        nullCallback nullCb = new nullCallback();
-        Object unsu2 = server1.subscribe(null, null, nullCb, "2nd sub");
-
-        int loops = 5;
-        while(loops-- > 0) {
-            //System.out.println("Send command to rc client");
-            server1.send(msg);
-            try {Thread.sleep(500);}
-            catch (InterruptedException e) {}
+        ConnectDisconnectThread(String host, int port) {
+            rcClientHost = host;
+            rcClientTcpPort = port;
         }
-        server1.unsubscribe(unsub);
-        server1.disconnect();
 
+        public void run() {
+            try {
+                String rcsUDL = "cMsg:rcs://" + rcClientHost + ":" + rcClientTcpPort;
 
-        // test reconnect feature
-        System.out.println("Now wait 2 seconds and connect again");
-        try {Thread.sleep(2000);}
-        catch (InterruptedException e) {}
-
-        cMsg server2 = new cMsg(rcsUDL, "rc server", "udp trial");
-        server2.connect();
-        server2.start();
-        server2.subscribe("subby", "typey", cb2, "2nd sub");
-
-        loops = 5;
-        while(loops-- > 0) {
-            server2.send(msg);
-            try {Thread.sleep(500);}
-            catch (InterruptedException e) {}
+                cMsg server = new cMsg(rcsUDL, "rc server", "connect/disconnect trial");
+//System.out.println("connect");
+                server.connect();
+                try {Thread.sleep(200);}
+                catch (InterruptedException e) {}
+                server.disconnect();
+            }
+            catch (cMsgException e) {
+                e.printStackTrace();
+            }
         }
-        
+
+    }
+
+    /**
+     * Class for client testing message sending after server disconnects then reconnects.
+     */
+    class RcServerReconnectThread extends Thread{
+
+        String rcClientHost;
+        int rcClientTcpPort;
+
+        RcServerReconnectThread(String host, int port) {
+            rcClientHost = host;
+            rcClientTcpPort = port;
+        }
+
+        public void run() {
+            try {
+                System.out.println("Starting RC Broadcast Server");
+                // RC Server domain UDL is of the form:
+                //       cMsg:rcs://<host>:<tcpPort>?port=<udpPort>
+                //
+                // The intial cMsg:rcs:// is stripped off by the top layer API
+                //
+                // Remember that for this domain:
+                // 1) host is NOT optional (must start with an alphabetic character according to "man hosts" or
+                //    may be in dotted form (129.57.35.21)
+                // 2) host can be "localhost"
+                // 3) tcp port is optional and defaults to 7654 (cMsgNetworkConstants.rcClientPort)
+                // 4) the udp port to listen on may be given by the optional port parameter.
+                //    if it's not given, the system assigns one
+                String rcsUDL = "cMsg:rcs://" + rcClientHost + ":" + rcClientTcpPort + "/?port=5859";
+
+                cMsg server = new cMsg(rcsUDL, "rc server", "udp trial");
+                server.connect();
+                server.start();
+
+                rcCallback cb2 = new rcCallback();
+                Object unsub = server.subscribe("subby", "typey", cb2, "1st sub");
+
+                nullCallback nullCb = new nullCallback();
+                Object unsub2 = server.subscribe(null, null, nullCb, "2nd sub");
+
+                // send stuff to rc client
+                cMsgMessage msg = new cMsgMessage();
+                msg.setSubject("rcSubject");
+                msg.setType("rcType");
+
+                int loops = 5;
+                while(loops-- > 0) {
+                    System.out.println("Send msg " +  (loops+1) + " to rc client");
+                    msg.setUserInt(loops+1);
+                    try {Thread.sleep(500);}
+                    catch (InterruptedException e) {}
+                    server.send(msg);
+                }
+                server.unsubscribe(unsub);
+                server.unsubscribe(unsub2);
+                server.disconnect();
+
+                // test reconnect feature
+                System.out.println("Now wait 2 seconds and connect again");
+                try {Thread.sleep(2000);}
+                catch (InterruptedException e) {}
+
+                cMsg server2 = new cMsg(rcsUDL, "rc server", "udp trial");
+                server2.connect();
+                server2.start();
+
+                server2.subscribe("subby", "typey", cb2, "3rd sub");
+                server2.subscribe(null, null, nullCb, "4th sub");
+
+                loops = 5;
+                while(loops-- > 0) {
+                    System.out.println("Send msg " +  (loops+1) + " to rc client");
+                    msg.setUserInt(loops+1);
+                    server2.send(msg);
+                    try {Thread.sleep(500);}
+                    catch (InterruptedException e) {}
+                }
+            }
+            catch (cMsgException e) {
+                e.printStackTrace();
+            }
+        }
 
     }
 
