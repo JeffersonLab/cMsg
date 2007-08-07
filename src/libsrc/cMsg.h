99d7ee114ba00449d2a5e3904bf3890173611abc
Carl Timmer <timmer@jlab.org>
2007-08-07 13:47:06 +0000

    add strncasecmp for vxworks

--- src/libsrc/cMsg.h
+++ src/libsrc/cMsg.h
@@ -127,6 +127,7 @@ extern "C" {
 #ifdef VXWORKS
   char *strdup(const char *s1);
   int   strcasecmp(const char *s1, const char *s2);
+  int   strncasecmp(const char *s1, const char *s2, size_t n);
 #endif
 
 
