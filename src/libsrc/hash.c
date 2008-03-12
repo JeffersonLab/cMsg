7a2c356517f50e09622d703d92f79e8622fc55bb
Carl Timmer <timmer@jlab.org>
2008-03-12 14:14:12 +0000

    fix some memory issues in the hash table

--- src/libsrc/hash.c
+++ src/libsrc/hash.c
@@ -28,14 +28,6 @@
 
 #define HASH_LIMIT 0.5
 
-/** This structure represents a hash table entry (key & value). */
-typedef struct hash_node_t {
-  void *data;                 /**< data in hash node. */
-  const char * key;           /**< key for hash lookup. */
-  struct hash_node_t *next;   /**< next node in hash chain. */
-} hash_node_t;
-
-
 /*
  * This hash function returns a hash number for a given key.
  *
@@ -43,19 +35,19 @@ typedef struct hash_node_t {
  * @param key The key to create a hash number for
  * @return hash value
  */
-static int hash(const hash_t *tptr, const char *key) {
-  int i=0;
-  int hashvalue;
- 
-  while (*key != '\0')
-    i=(i<<3)+(*key++ - '0');
- 
-  hashvalue = (((i*1103515249)>>tptr->downshift) & tptr->mask);
-  if (hashvalue < 0) {
-    hashvalue = 0;
-  }    
+static int hash(const hashTable *tptr, const char *key) {
+    int i=0;
+    int hashvalue;
 
-  return hashvalue;
+    while (*key != '\0')
+        i=(i<<3)+(*key++ - '0');
+
+    hashvalue = (((i*1103515249)>>tptr->downshift) & tptr->mask);
+    if (hashvalue < 0) {
+        hashvalue = 0;
+    }    
+
+    return hashvalue;
 }
 
 
@@ -65,29 +57,29 @@ static int hash(const hash_t *tptr, const char *key) {
  * @param tptr Pointer to the hash table to initialize
  * @param buckets The number of initial buckets to create
  */
-void hashInit(hash_t *tptr, int buckets) {
+void hashInit(hashTable *tptr, int buckets) {
 
-  /* make sure we allocate something */
-  if (buckets==0)
-    buckets=16;
+    /* make sure we allocate something */
+    if (buckets==0)
+        buckets=16;
 
-  /* initialize the table */
-  tptr->entries=0;
-  tptr->size=2;
-  tptr->mask=1;
-  tptr->downshift=29;
+    /* initialize the table */
+    tptr->entries=0;
+    tptr->size=2;
+    tptr->mask=1;
+    tptr->downshift=29;
 
-  /* ensure buckets is a power of 2 */
-  while (tptr->size<buckets) {
-    tptr->size<<=1;
-    tptr->mask=(tptr->mask<<1)+1;
-    tptr->downshift--;
-  } /* while */
+    /* ensure buckets is a power of 2 */
+    while (tptr->size<buckets) {
+        tptr->size<<=1;
+        tptr->mask=(tptr->mask<<1)+1;
+        tptr->downshift--;
+    } /* while */
 
-  /* allocate memory for table */
-  tptr->bucket=(hash_node_t **) calloc(tptr->size, sizeof(hash_node_t *));
+    /* allocate memory for table */
+    tptr->bucket=(hashNode **) calloc(tptr->size, sizeof(hashNode *));
 
-  return;
+    return;
 }
 
 
@@ -96,31 +88,31 @@ void hashInit(hash_t *tptr, int buckets) {
  *
  * @param tptr Pointer to a hash table
  */
-static void rebuild_table(hash_t *tptr) {
-  hash_node_t **old_bucket, *old_hash, *tmp;
-  int old_size, h, i;
+static void rebuild_table(hashTable *tptr) {
+    hashNode **old_bucket, *old_hash, *tmp;
+    int old_size, h, i;
 
-  old_bucket=tptr->bucket;
-  old_size=tptr->size;
+    old_bucket=tptr->bucket;
+    old_size=tptr->size;
 
-  /* create a new table and rehash old buckets */
-  hashInit(tptr, old_size<<1);
-  for (i=0; i<old_size; i++) {
-    old_hash=old_bucket[i];
-    while(old_hash) {
-      tmp=old_hash;
-      old_hash=old_hash->next;
-      h=hash(tptr, tmp->key);
-      tmp->next=tptr->bucket[h];
-      tptr->bucket[h]=tmp;
-      tptr->entries++;
-    } /* while */
-  } /* for */
+    /* create a new table and rehash old buckets */
+    hashInit(tptr, old_size<<1);
+    for (i=0; i<old_size; i++) {
+        old_hash=old_bucket[i];
+        while(old_hash) {
+            tmp=old_hash;
+            old_hash=old_hash->next;
+            h=hash(tptr, tmp->key);
+            tmp->next=tptr->bucket[h];
+            tptr->bucket[h]=tmp;
+            tptr->entries++;
+        } /* while */
+    } /* for */
 
-  /* free memory used by old table */
-  free(old_bucket);
+    /* free memory used by old table */
+    free(old_bucket);
 
-  return;
+    return;
 }
 
 
@@ -133,24 +125,24 @@ static void rebuild_table(hash_t *tptr) {
  * @param data Pointer which gets filled with entry in hash table
  * @return 1 if entry found, else 0
  */
-int hashLookup(const hash_t *tptr, const char *key, void **data) {
-  int h;
-  hash_node_t *node;
+int hashLookup(const hashTable *tptr, const char *key, void **data) {
+    int h;
+    hashNode *node;
 
-  /* find the entry in the hash table */
-  h=hash(tptr, key);
-  for (node=tptr->bucket[h]; node!=NULL; node=node->next) {
-    if (!strcmp(node->key, key))
-      break;
-  }
+    /* find the entry in the hash table */
+    h=hash(tptr, key);
+    for (node=tptr->bucket[h]; node!=NULL; node=node->next) {
+        if (!strcmp(node->key, key))
+            break;
+    }
 
-  /* Set pointer to the entry if it exists. Return 1 if it exists, else 0. */
-  if (node != NULL) {
-      if (data != NULL)
-          *data = node->data;
-      return (1);
-  }
-  return (0);
+    /* Set pointer to the entry if it exists. Return 1 if it exists, else 0. */
+    if (node != NULL) {
+        if (data != NULL)
+            *data = node->data;
+        return (1);
+    }
+    return (0);
 }
 
 
@@ -168,43 +160,43 @@ int hashLookup(const hash_t *tptr, const char *key, void **data) {
  * @return 0 if the only the new entry is inserted or 1 if the new entry is
  *         replacing an existing entry
  */
-int hashInsert(hash_t *tptr, const char *key, void *data, void **oldData) {
-  hash_node_t *node;
-  int h;
-  
-  /*****************************************************/
-  /* check to see if the entry exists, if so update it */
-  
-  /* find the entry in the hash table */
-  h=hash(tptr, key);
-  for (node=tptr->bucket[h]; node!=NULL; node=node->next) {
-    if (!strcmp(node->key, key))
-      break;
-  }
+int hashInsert(hashTable *tptr, const char *key, void *data, void **oldData) {
+    hashNode *node;
+    int h;
 
-  /* if entry exists, update it and return */
-  if (node != NULL) {
-      if (oldData != NULL)
-          *oldData = node->data;
-      node->data = data;
-      return (1);
-  }
-  /*****************************************************/
+    /*****************************************************/
+    /* check to see if the entry exists, if so update it */
 
-  /* expand the table if needed */
-  while (tptr->entries>=HASH_LIMIT*tptr->size) {
-   rebuild_table(tptr);
-  }
+    /* find the entry in the hash table */
+    h=hash(tptr, key);
+    for (node=tptr->bucket[h]; node!=NULL; node=node->next) {
+        if (!strcmp(node->key, key))
+            break;
+    }
 
-  /* insert the new entry */
-  node=(struct hash_node_t *) malloc(sizeof(hash_node_t));
-  node->data=data;
-  node->key=key;
-  node->next=tptr->bucket[h];
-  tptr->bucket[h]=node;
-  tptr->entries++;
+    /* if entry exists, update it and return */
+    if (node != NULL) {
+        if (oldData != NULL)
+            *oldData = node->data;
+        node->data = data;
+        return (1);
+    }
+    /*****************************************************/
 
-  return (0);
+    /* expand the table if needed */
+    while (tptr->entries>=HASH_LIMIT*tptr->size) {
+        rebuild_table(tptr);
+    }
+
+    /* insert the new entry */
+    node=(struct hash_node_t *) malloc(sizeof(hashNode));
+    node->data=data;
+    node->key=strdup(key); /* copy key to avoid it going out of scope */
+    node->next=tptr->bucket[h];
+    tptr->bucket[h]=node;
+    tptr->entries++;
+
+    return (0);
 }
 
 
@@ -218,106 +210,177 @@ int hashInsert(hash_t *tptr, const char *key, void *data, void **oldData) {
  * 
  * @return 1 if entry removed, else 0
 */
-int hashRemove(hash_t *tptr, const char *key, void **data) {
-  hash_node_t *node, *last;
-  int h;
+int hashRemove(hashTable *tptr, const char *key, void **data) {
+    hashNode *node, *last;
+    int h;
 
-  /* find the node to remove */
-  h=hash(tptr, key);
-  for (node=tptr->bucket[h]; node; node=node->next) {
-    if (!strcmp(node->key, key))
-      break;
-  }
-
-  /* Didn't find anything */
-  if (node==NULL)
-    return 0;
-
-  /* if node is at head of bucket, we have it easy */
-  if (node==tptr->bucket[h])
-    tptr->bucket[h]=node->next;
-  else {
-    /* find the node before the node we want to remove */
-    for (last=tptr->bucket[h]; last && last->next; last=last->next) {
-      if (last->next==node)
-        break;
+    /* find the node to remove */
+    h=hash(tptr, key);
+    for (node=tptr->bucket[h]; node; node=node->next) {
+        if (!strcmp(node->key, key))
+            break;
     }
-    last->next=node->next;
-  }
 
-  /* free memory and return the data */
-  if (data != NULL)
-      *data=node->data;
-  free(node);
-  tptr->entries--;
+    /* Didn't find anything */
+    if (node==NULL)
+        return 0;
 
-  return (1);
-}
-
-
-/*
- * This routine clears the table of all entries.
- * It returns an array of all the entries and its size.
- * 
- * @param tptr A pointer to the hash table
- * @param data pointer to array of void pointers -
- *             gets filled with entries if not NULL
- * @param size pointer to int -
- *             gets filled with the number of entries if not NULL and data not NULL
- */
-void hashClear(hash_t *tptr, void ***data, int *size) {
-  hash_node_t *node, *last;
-  int i, index=0;
-  void **array=NULL;
-  
-  /* return data so user can free it */
-  if (data != NULL) {
-    if (tptr->entries > 0) {
-      array = (void **) malloc(tptr->entries*sizeof(void *));
-      *data = array;
-      if (size != NULL)
-         *size = tptr->entries;
-    }
+    /* if node is at head of bucket, we have it easy */
+    if (node==tptr->bucket[h])
+        tptr->bucket[h]=node->next;
     else {
-        *data = NULL;
-        if (size != NULL)
-            *size = 0;
+        /* find the node before the node we want to remove */
+        for (last=tptr->bucket[h]; last && last->next; last=last->next) {
+            if (last->next==node)
+                break;
+        }
+        last->next=node->next;
     }
-  }
-  
-  for (i=0; i<tptr->size; i++) {
-    node = tptr->bucket[i];
-    while (node != NULL) { 
-      last = node;   
-      node = node->next;
-      if (array != NULL)
-          array[index++] = last->data;
-      free(last);
-    }
-  }
-  
-  tptr->entries = 0;
+
+    /* free memory and return the data */
+    if (data != NULL)
+        *data=node->data;
+    free(node->key);
+    free(node);
+    tptr->entries--;
+
+    return (1);
 }
 
 
 /*
- * This routine deletes the entire table, and all entries.
- * It returns an array of all the entries and its size.
+ * This routine returns an array of all the entries and its size.
+ * The caller must free the returned array to avoid a memory leak,
+ * but must NOT free the keys or data.
  * 
  * @param tptr A pointer to the hash table
- * @param data pointer to array of void pointers -
- *             gets filled with entries if not NULL
+ * @param entries pointer to array of hashNode structures -
+ *                gets filled with entries if not NULL
  * @param size pointer to int -
- *             gets filled with the number of entries if not NULL and data not NULL
+ *             gets filled with the number of entries if not NULL and entries not NULL
  */
-void hashDestroy(hash_t *tptr, void ***data, int *size) {
-  hashClear(tptr, data, size);
+void hashGetAll(hashTable *tptr, hashNode **entries, int *size) {
+    hashNode *node, *last;
+    int i, index=0;
+    hashNode *array=NULL;
 
-  /* free the entire array of buckets */
-  if (tptr->bucket != NULL) {
-    free(tptr->bucket);
-    memset(tptr, 0, sizeof(hash_t));
-  }
+    /* return data so user can free it */
+    if (entries != NULL) {
+        if (tptr->entries > 0) {
+            array = (hashNode *) malloc(tptr->entries*sizeof(hashNode));
+            *entries = array;
+            if (size != NULL)
+                *size = tptr->entries;
+        }
+        else {
+            *entries = NULL;
+            if (size != NULL)
+                *size = 0;
+        }
+    }
+
+    for (i=0; i<tptr->size; i++) {
+        node = tptr->bucket[i];
+        while (node != NULL) { 
+            last = node;   
+            node = node->next;
+            if (array != NULL) {
+                array[index].key    = last->key;
+                array[index].data   = last->data;
+                array[index++].next = NULL;
+            }
+        }
+    }
+}
+
+
+/*
+ * This routine clears the table of all entries.<p>
+ * If the "entries" arg is not NULL, it returns an array of
+ * all the entries and its size, and the caller must free
+ * the "key" member of each hashNode to avoid a memory leak.
+ * The caller must also free the returned array.
+ * The caller alone knows what to do with the "data" member.
+ * If the table is empty, then "entries" points to NULL and
+ * size is set to zero.<p>
+ * If the "entries" arg is NULL, then this routine frees all
+ * the keys and all data is ignored. If data point to allocated
+ * memory, it may be lost.
+ * 
+ * @param tptr A pointer to the hash table
+ * @param entries pointer to array of hashNode structures -
+ *                gets filled with entries if not NULL
+ * @param size pointer to int -
+ *             gets filled with the number of entries if not NULL and entries not NULL
+ */
+void hashClear(hashTable *tptr, hashNode **entries, int *size) {
+    hashNode *node, *last;
+    int i, index=0;
+    hashNode *array=NULL;
+
+    /* return data so user can free it */
+    if (entries != NULL) {
+        if (tptr->entries > 0) {
+            array = (hashNode *) malloc(tptr->entries*sizeof(hashNode));
+            *entries = array;
+            if (size != NULL)
+                *size = tptr->entries;
+        }
+        else {
+            *entries = NULL;
+            if (size != NULL)
+                *size = 0;
+        }
+    }
+
+    for (i=0; i<tptr->size; i++) {
+        node = tptr->bucket[i];
+        while (node != NULL) { 
+            last = node;   
+            node = node->next;
+            if (array != NULL) {
+                array[index].key    = last->key;
+                array[index].data   = last->data;
+                array[index++].next = NULL;
+            }
+            else {
+                free(last->key);
+            }
+            free(last);
+        }
+    }
+
+    tptr->entries = 0;
+}
+
+
+/*
+ * This routine deletes the entire table, and all entries.<p>
+ * If the "entries" arg is not NULL, it returns an array of
+ * all the entries and its size, and the caller must free
+ * the "key" member of each hashNode to avoid a memory leak.
+ * The caller must also free the returned array.
+ * The caller alone knows what to do with the "data" member.
+ * If the table is empty, then "entries" points to NULL and
+ * size is set to zero.<p>
+ * If the "entries" arg is NULL, then this routine frees all
+ * the keys and all data is ignored. If data point to allocated
+ * memory, it may be lost.
+ * 
+ * @param tptr A pointer to the hash table
+ * @param entries pointer to array of hashNode structures -
+ *                gets filled with entries if not NULL
+ * @param size pointer to int -
+ *             gets filled with the number of entries if not NULL and entries not NULL
+ */
+void hashDestroy(hashTable *tptr, hashNode **entries, int *size) {
+    hashClear(tptr, entries, size);
+
+    /* free the entire array of buckets */
+    if (tptr->bucket != NULL) {
+        free(tptr->bucket);
+        memset(tptr, 0, sizeof(hashTable));
+    }
 }
 
 
@@ -327,7 +390,7 @@ void hashDestroy(hash_t *tptr, void ***data, int *size) {
  * @param tptr A pointer to the hash table
  * @return the number of entries
  */
-int hashSize(hash_t *tptr) {
+int hashSize(hashTable *tptr) {
     return tptr->entries;  
 }
 
@@ -338,19 +401,18 @@ int hashSize(hash_t *tptr) {
  * @param tptr A pointer to the hash table
  * @return average length of search 
  */
-static float alos(hash_t *tptr) {
-  int i,j;
-  float alos=0;
-  hash_node_t *node;
+static float alos(hashTable *tptr) {
+    int i,j;
+    float alos=0;
+    hashNode *node;
 
+    for (i=0; i<tptr->size; i++) {
+        for (node=tptr->bucket[i], j=0; node!=NULL; node=node->next, j++);
+        if (j)
+            alos+=((j*(j+1))>>1);
+    } /* for */
 
-  for (i=0; i<tptr->size; i++) {
-    for (node=tptr->bucket[i], j=0; node!=NULL; node=node->next, j++);
-    if (j)
-      alos+=((j*(j+1))>>1);
-  } /* for */
-
-  return(tptr->entries ? alos/tptr->entries : 0);
+    return(tptr->entries ? alos/tptr->entries : 0);
 }
 
 
@@ -360,13 +422,13 @@ static float alos(hash_t *tptr) {
  * @param tptr A pointer to the hash table
  * @return string with hash table statistics 
  */
-char *hashStats(hash_t *tptr) {
-  static char buf[1024];
+char *hashStats(hashTable *tptr) {
+    static char buf[1024];
 
-  sprintf(buf, "%u slots, %u entries, and %1.2f ALOS",
-    (int)tptr->size, (int)tptr->entries, alos(tptr));
+    sprintf(buf, "%u slots, %u entries, and %1.2f ALOS",
+            (int)tptr->size, (int)tptr->entries, alos(tptr));
 
-  return(buf);
+    return(buf);
 }
 
 
