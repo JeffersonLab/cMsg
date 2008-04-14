549b5ec7d748e390ada6529ad1edeea7a081bc54
Carl Timmer <timmer@jlab.org>
2008-04-14 17:04:39 +0000

    debug printout added

--- src/examples/producer.c
+++ src/examples/producer.c
@@ -67,7 +67,7 @@ int main(int argc,char **argv) {
   void *msg, *domainId;
   
   /* msg rate measuring variables */
-  int             dostring=1, count, delay=1, loops=20000, ignore=0;
+  int             dostring=1, count, delay=0, loops=200000, ignore=0;
   struct timespec t1, t2, sleeep;
   double          freq, freqAvg=0., deltaT, totalT=0.;
   long long       totalC=0;
@@ -141,6 +141,7 @@ int main(int argc,char **argv) {
            return(-1);
          }
          UDL = argv[++i];
+printf("Setting UDP = %s\n", UDL);
        }
        else if (strcmp(argv[i], "-h") == 0) {
          usage();
@@ -175,7 +176,7 @@ int main(int argc,char **argv) {
   msg = cMsgCreateMessage();    /* allocating mem here */
   cMsgSetSubject(msg, subject); /* allocating mem here */
   cMsgSetType(msg, type);       /* allocating mem here */
-  /* cMsgSetReliableSend(msg, 0);*/
+/*  cMsgSetReliableSend(msg, 0);*/
   
   /* set compound payload fields */
 
