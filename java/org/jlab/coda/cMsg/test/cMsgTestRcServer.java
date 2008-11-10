1482cf11b20eb78aa714dddab4af7c628f2317d6
Carl Timmer <timmer@jlab.org>
2008-11-10 21:35:55 +0000

    clean up

--- java/org/jlab/coda/cMsg/test/cMsgTestRcServer.java
+++ java/org/jlab/coda/cMsg/test/cMsgTestRcServer.java
@@ -14,7 +14,7 @@
  *                                                                            *
  *----------------------------------------------------------------------------*/
 
-package org.jlab.coda.cMsg.apps;
+package org.jlab.coda.cMsg.test;
 
 import org.jlab.coda.cMsg.cMsg;
 import org.jlab.coda.cMsg.cMsgCallbackAdapter;
@@ -25,25 +25,23 @@ import java.util.concurrent.CountDownLatch;
 import java.util.concurrent.TimeoutException;
 
 /**
- * Simulates an rc broadcast server and rc server together.
+ * Simulates an rc Multicast server and an rc server together.
  */
 public class cMsgTestRcServer {
-    String rcClientHost;
-    int rcClientTcpPort;
-    String name;
-    CountDownLatch latch;
-    int count;
-    cMsg cmsg;
 
-    class BroadcastCallback extends cMsgCallbackAdapter {
+    private String rcClientHost;
+    private int rcClientTcpPort;
+    private CountDownLatch latch;
+    private int count;
+
+    class MulticastCallback extends cMsgCallbackAdapter {
          public void callback(cMsgMessage msg, Object userObject) {
              rcClientHost = msg.getSenderHost();
              rcClientTcpPort = msg.getUserInt();
-             name = msg.getSender();
 
-             System.out.println("RAN CALLBACK, host = " + rcClientHost +
+             System.out.println("Running RC Multicast domain callback, host = " + rcClientHost +
                                 ", port = " + rcClientTcpPort +
-                                ", name = " + name);
+                                ", name = " + msg.getSender());
 
              // finish connection
              latch.countDown();
@@ -54,19 +52,7 @@ public class cMsgTestRcServer {
     class rcCallback extends cMsgCallbackAdapter {
         public void callback(cMsgMessage msg, Object userObject) {
             count++;
-            String s = (String) userObject;
-            System.out.println("RAN CALLBACK, for regular subscription");
-            System.out.println("              msg text = " + msg.getText());
-            System.out.println("              msg  sub = " + msg.getSubject());
-            System.out.println("              msg type = " + msg.getType());
-            msg.payloadPrintout(0);
-        }
-    }
-
-    class nullCallback extends cMsgCallbackAdapter {
-        public void callback(cMsgMessage msg, Object userObject) {
-            count++;
-            System.out.println("RAN CALLBACK, for NULL subscription");
+            System.out.println("Running RC Server domain callback");
             System.out.println("              msg text = " + msg.getText());
             System.out.println("              msg  sub = " + msg.getSubject());
             System.out.println("              msg type = " + msg.getType());
@@ -87,23 +73,26 @@ public class cMsgTestRcServer {
 
 
     public void run() throws cMsgException {
-        System.out.println("Starting RC Broadcast Server");
 
-        // RC Broadcast domain UDL is of the form:
-        //       cMsg:rcb://<udpPort>?expid=<expid>
+        System.out.println("Starting RC Multicast Domain test server");
+
+        // RC Multicast domain UDL is of the form:
+        //       cMsg:rcm://<udpPort>?expid=<expid>
         //
-        // The intial cMsg:rcb:// is stripped off by the top layer API
+        // The intial cMsg:rcm:// is stripped off by the top layer API
         //
         // Remember that for this domain:
-        // 1) udp listening port is optional and defaults to 6543 (cMsgNetworkConstants.rcMulticastPort)
+        // 1) udp listening port is optional and defaults to cMsgNetworkConstants.rcMulticastPort
         // 2) the experiment id is given by the optional parameter expid. If none is
         //    given, the environmental variable EXPID is used. if that is not defined,
-        //    an exception is thrown
+        //    an exception is thrown.
 
-        String UDL = "cMsg:rcb://?expid=carlExp";
+        String UDL = "cMsg:rcm://?expid=carlExp";
 
-        cmsg = new cMsg(UDL, "broadcast listener", "udp trial");
-        try {cmsg.connect();}
+        // start up rc multicast server
+        cMsg cmsg = new cMsg(UDL, "multicast listener", "udp trial");
+        try {
+            cmsg.connect();}
         catch (cMsgException e) {
             System.out.println(e.getMessage());
             return;
@@ -112,19 +101,21 @@ public class cMsgTestRcServer {
         // enable message reception
         cmsg.start();
 
-        BroadcastCallback cb = new BroadcastCallback();
+        // create a callback for when a client connects to the rc multicast server
+        MulticastCallback cb = new MulticastCallback();
         // subject and type are ignored in this domain
         cmsg.subscribe("sub", "type", cb, null);
 
-        // wait for incoming message from rc client
+        // wait for first incoming message from rc client
         try { latch.await(); }
         catch (InterruptedException e) {}
 
+        // That was the RC multicast domain part,
+        // now we go on the RC server domain part.
 
-
-        // now that we have the message, we know what TCP host and port
+        // Now that we have a message, we know what TCP host and port
         // to connect to in the RC Server domain.
-        System.out.println("Starting RC Server");
+        System.out.println("Starting RC Server Domain test server");
 
         //try {Thread.sleep(4000);}
         //catch (InterruptedException e) {}
@@ -138,21 +129,22 @@ public class cMsgTestRcServer {
         // 1) host is NOT optional (must start with an alphabetic character according to "man hosts" or
         //    may be in dotted form (129.57.35.21)
         // 2) host can be "localhost"
-        // 3) tcp port is optional and defaults to 7654 (cMsgNetworkConstants.rcClientPort)
+        // 3) tcp port is optional and defaults to cMsgNetworkConstants.rcClientPort
         // 4) the udp port to listen on may be given by the optional port parameter.
         //    if it's not given, the system assigns one
-        String rcsUDL = "cMsg:rcs://" + rcClientHost + ":" + rcClientTcpPort + "/?port=5859";
+        
+        String rcsUDL = "cMsg:rcs://" + rcClientHost + ":" + rcClientTcpPort;
 
+        // Connect to the RC client that just connected with our RC multicast server,
+        // and thereby conclude the client's complicated connection process.
         cMsg server = new cMsg(rcsUDL, "rc server", "udp trial");
         server.connect();
 
         server.start();
 
+        // register callback to get messages from rc client
         rcCallback cb2 = new rcCallback();
-        Object unsub = server.subscribe("subby", "typey", cb2, "1st sub");
-
-        nullCallback nullCb = new nullCallback();
-        Object unsu2 = server.subscribe(null, null, nullCb, "2nd sub");
+        Object unsub = server.subscribe("subby", "typey", cb2, null);
 
         System.out.println("wait 2 sec");
         try {Thread.sleep(2000);}
@@ -165,7 +157,7 @@ public class cMsgTestRcServer {
         msg.setType("rcType");
 
         server.send(msg);
-        System.out.println("Sent msg (sub = rcSubject, typ = rcType) to rc client, wait 2 ...\n");
+        System.out.println("Sent msg (sub = rcSubject, typ = rcType) to rc client, wait 2 sec ...\n");
         try {Thread.sleep(2000);}
         catch (InterruptedException e) {}
 
