fa845cdd3172b36511c7cb51b9a8ed9002832de4
Carl Timmer <timmer@jlab.org>
2009-05-07 18:28:18 +0000

    can receive its own messages and delay inbetween sends

--- src/examples/producer.c
+++ src/examples/producer.c
@@ -30,16 +30,51 @@
 
 #include "cMsg.h"
 
+  void *msg,*msg2, *msgs[2], *domainId;
+  int delay=0, count=0;
 
 /******************************************************************/
 static void usage() {
-    printf("Usage:  producer [-a <size> | -b <size>] -u <UDL> -s <subject> -t <type> -d\n");
+    printf("Usage:  producer [-a <size> | -b <size>] -n <name> -u <UDL> -s <subject> -t <type> -delay <delay_in_sec> -d\n");
     printf("                  -a sets the byte size for ascii data, or\n");
     printf("                  -b sets the byte size for binary data\n");
     printf("                  -s sets the subscription subject\n");
     printf("                  -t sets the subscription type\n");
+    printf("                  -n sets the client name\n");
     printf("                  -u sets the connection UDL\n");
     printf("                  -d turns on debug output\n");
+    printf("                  -delay sets the delay between sends in seconds\n");
+}
+
+/******************************************************************/
+static void callback(void *msg, void *arg) {  
+  int err;
+  char *p;
+
+  count++;
+  
+  cMsgToString(msg, &p);
+  printf("XML message:\n%s", p);
+  free(p);
+
+  /*
+  printf("\nCallback msg payload:\n");
+  cMsgPayloadPrint(msg);
+  */
+  
+  /*
+  if (delay != 0) {
+  nanosleep(&sleeep, NULL);
+  }
+
+  err = cMsgSend(domainId, msg);
+  if (err != CMSG_OK) {
+     printf("cMsgSend: err = %d, %s\n",err, cMsgPerror(err));
+     return;
+  }
+  */
+  /* user MUST free messages passed to the callback */
+  cMsgFreeMessage(&msg);
 }
 
 /******************************************************************/
@@ -51,56 +86,69 @@ int main(int argc,char **argv) {
   char *type          = "TYPE";
   char *text          = "JUNK";
   char *bytes         = NULL;
-  char *UDL           = "cMsg:cmsg://localhost:3456/cMsg/test";
+  char *UDL           = "cMsg://localhost/cMsg/myNameSpace";
   char *p;
   int   i, j, err, debug=0, msgSize=0, mainloops=2000;
-//  int8_t    i1vals[3];
-//  int16_t   i2vals[3];
-//  int32_t   i3vals[3];
-//  int64_t   i4vals[3];
-//  uint8_t   i5vals[3];
-//  uint16_t  i6vals[3];
-//  uint32_t  i7vals[3];
-//  uint64_t  i8vals[3];
-//  char       *vals[3];
-//  float      fvals[3];
-//  double     dvals[3];
-  void *msg, *domainId;
-  
+  int8_t    i1vals[3];
+  int16_t   i2vals[3];
+  int32_t   i3vals[3];
+  int64_t   i4vals[3];
+  uint8_t   i5vals[3];
+  uint16_t  i6vals[3];
+  uint32_t  i7vals[3];
+  uint64_t  i8vals[3];
+  char       *vals[3];
+  float      fvals[3];
+  double     dvals[3];
+  char      *binArray;
+  cMsgSubscribeConfig *config;
+  void *unSubHandle;
+
   /* msg rate measuring variables */
-  int             dostring=1, count, delay=0, loops=200000, ignore=0;
-  struct timespec t1, t2, sleeep;
+  int             dostring=1, loops=200000, ignore=0;
+  struct timespec t1, t2, sleeep = {0,0};
   double          freq, freqAvg=0., deltaT, totalT=0.;
   long long       totalC=0;
-  
-  sleeep.tv_sec  = 3; /* 3 sec */
-  sleeep.tv_nsec = 0;
-  
+    
   if (argc > 1) {
   
     for (i=1; i<argc; i++) {
 
-       if (strcmp("-a", argv[i]) == 0) {
-         if (argc < i+2) {
-           usage();
-           return(-1);
-         }
+        if (strcmp("-delay", argv[i]) == 0) {
+            if (argc < i+2) {
+                usage();
+                return(-1);
+            }
          
-         dostring = 1;
+            delay = atoi(argv[++i]);
+            if (delay < 0) {
+                printf("Delay must be >= 0\n\n");
+                usage();
+                return(-1);
+            }
+            sleeep.tv_sec = delay;
+            /*printf("Delay = %d\n", delay);*/
+        }
+        else if (strcmp("-a", argv[i]) == 0) {
+            if (argc < i+2) {
+                usage();
+                return(-1);
+            }
          
-         msgSize = atoi(argv[++i]);
-         text = p = (char *) malloc((size_t) (msgSize + 1));
-         if (p == NULL) exit(1);
-printf("using text msg size %d\n", msgSize);
-         for (j=0; j < msgSize; j++) {
-           *p = 'A';
-           p++;
-         }
-         *p = '\0';
-         /*printf("Text = %s\n", text);*/
+            dostring = 1;
          
-       }
-       else if (strcmp("-b", argv[i]) == 0) {
+            msgSize = atoi(argv[++i]);
+            text = p = (char *) malloc((size_t) (msgSize + 1));
+            if (p == NULL) exit(1);
+            for (j=0; j < msgSize; j++) {
+                *p = 'A';
+                p++;
+            }
+            *p = '\0';
+            /*printf("Text = %s\n", text);*/
+         
+        }
+        else if (strcmp("-b", argv[i]) == 0) {
          if (argc < i+2) {
            usage();
            return(-1);
@@ -113,7 +161,6 @@ printf("using text msg size %d\n", msgSize);
            usage();
            return(-1);
          }
-printf("using array msg size %d\n", msgSize);
          bytes = p = (char *) malloc((size_t) msgSize);
          if (p == NULL) exit(1);
          for (j=0; j < msgSize; j++) {
@@ -121,6 +168,13 @@ printf("using array msg size %d\n", msgSize);
            p++;
          }
 
+       }
+       else if (strcmp(argv[i], "-n") == 0) {
+         if (argc < i+2) {
+           usage();
+           return(-1);
+         }
+         myName = argv[++i];
        }
        else if (strcmp(argv[i], "-s") == 0) {
          if (argc < i+2) {
@@ -142,7 +196,6 @@ printf("using array msg size %d\n", msgSize);
            return(-1);
          }
          UDL = argv[++i];
-printf("Setting UDL = %s\n", UDL);
        }
        else if (strcmp(argv[i], "-d") == 0) {
          debug = 1;
@@ -174,97 +227,136 @@ printf("Setting UDL = %s\n", UDL);
       exit(1);
   }
   
+  /* start receiving messages */
+  cMsgReceiveStart(domainId);
+  
+  /* set the subscribe configuration */
+  /*
+  config = cMsgSubscribeConfigCreate();
+  cMsgSetDebugLevel(CMSG_DEBUG_INFO);
+  */
+  /* subscribe - receive own messages */
+  /*
+  err = cMsgSubscribe(domainId, subject, type, callback, NULL, config, &unSubHandle);
+  if (err != CMSG_OK) {
+      if (debug) {
+          printf("cMsgSubscribe: %s\n",cMsgPerror(err));
+      }
+      exit(1);
+  }
+  cMsgSubscribeConfigDestroy(config);
+  */
+  
   /* create message to be sent */
   msg = cMsgCreateMessage();    /* allocating mem here */
   cMsgSetSubject(msg, subject); /* allocating mem here */
   cMsgSetType(msg, type);       /* allocating mem here */
-  /*cMsgSetReliableSend(msg, 0);*/
-  
-  /* set compound payload fields */
+  cMsgSetText(msg, "~!@#$%^&*()_-+=,.<>?/|\\;:[]{}`");
+  cMsgSetHistoryLengthMax(msg, 4);
 
-//  cMsgAddFloat (msg, "flt", -1.23456e-05);
-//  cMsgAddDouble(msg, "dbl", -1.23456789101012e-195);
-//  
-//  cMsgAddString (msg, "str", "string");
-//  
-//  vals[0] = "zero";
-//  vals[1] = "one";
-//  vals[2] = "two";
-//  cMsgAddStringArray(msg, "strA", (const char **) vals, 3);
-//
-//  fvals[0] = -1234567;
-//  fvals[1] = +234.;
-//  fvals[2] = -12.3456e-35;
-//  cMsgAddFloatArray(msg,  "fltA", fvals, 3);
-//  
-//  dvals[0] = -1234567;
-//  dvals[1] = +234.678910;
-//  dvals[2] = -12.3456789101012e-301;
-//  cMsgAddDoubleArray(msg,  "dblA", dvals, 3);
-//
-//  cMsgAddInt8   (msg, "int8",  SCHAR_MIN);
-//  cMsgAddInt16  (msg, "int16", SHRT_MIN);
-//  cMsgAddInt32  (msg, "int32", INT_MIN);
-//  cMsgAddInt64  (msg, "int64", -9223372036854775807LL);
-//  
-//  cMsgAddUint8   (msg, "uint8",  UCHAR_MAX);
-//  cMsgAddUint16  (msg, "uint16", USHRT_MAX);
-//  cMsgAddUint32  (msg, "uint32", UINT_MAX);
-//  cMsgAddUint64  (msg, "uint64", 18446744073709551615ULL);
-//    
-//  i1vals[0] = -128;
-//  i1vals[1] = 127;
-//  i1vals[2] = 255;
-//  cMsgAddInt8Array(msg, "int8A", i1vals, 3);
-//  
-//  i2vals[0] = -32768;
-//  i2vals[1] = 32767;
-//  i2vals[2] = 65535;
-//  cMsgAddInt16Array(msg, "int16A", i2vals, 3);
-//  
-//  i3vals[0] = -2147483648;
-//  i3vals[1] = 2147483647;
-//  i3vals[2] = 4294967295;
-//  cMsgAddInt32Array(msg, "int32A", i3vals, 3);
-//  
-//  i4vals[0] = -9223372036854775807LL;
-//  i4vals[1] = 9223372036854775807LL;
-//  i4vals[2] = 18446744073709551615ULL;
-//  cMsgAddInt64Array(msg, "int64A", i4vals, 3);
-//  
-//  i5vals[0] = 0;
-//  i5vals[1] = 255;
-//  i5vals[2] = -1;
-//  cMsgAddUint8Array(msg, "uint8A", i5vals, 3);
-//
-//  i6vals[0] = 0;
-//  i6vals[1] = 65535;
-//  i6vals[2] = -1;
-//  cMsgAddUint16Array(msg, "uint16A", i6vals, 3);
-//  
-//  i7vals[0] = 0U;
-//  i7vals[1] = 4294967295U;
-//  i7vals[2] = -1;
-//  cMsgAddUint32Array(msg, "uint32A", i7vals, 3);
-//  
-//  i8vals[0] = 0ULL;
-//  i8vals[1] = 18446744073709551615ULL;
-//  i8vals[2] = -1;
-//  cMsgAddUint64Array(msg, "uint64A", i8vals, 3);
-//  
-//  cMsgToString(msg, &p, 1);
-//  printf("XML message:\n%s", p);
-//  free(p);
-//    
-  if (dostring) {
-    printf("  try setting text to %s\n", text);
-    cMsgSetText(msg, text);
+  /*cMsgSetReliableSend(msg, 0);*/
+
+  /* set compound payload fields */
+  binArray = (char *) malloc(256);
+  for (j=0; j < 256; j++) {
+      binArray[j] = (char)j%255;
   }
-  else {
-    printf("  setting byte array\n");
-    cMsgSetByteArrayNoCopy(msg, bytes, msgSize);
-  }
-   
+ cMsgAddBinary(msg, "binaryArray", binArray, 58, CMSG_ENDIAN_LOCAL);
+ cMsgSetByteArrayNoCopy(msg, binArray, 58);
+
+ cMsgAddFloat (msg, "flt", -1.23456e-05);
+ cMsgAddDouble(msg, "dbl", -1.23456789101012e-195);
+
+ cMsgAddString (msg, "str", "string");
+
+ vals[0] = "zero\nzero";
+ vals[1] = "one\none";
+ vals[2] = "two\ntwo";
+ cMsgAddStringArray(msg, "strA", (const char **) vals, 3);
+
+ fvals[0] = -1234567;
+ fvals[1] = +234.;
+ fvals[2] = -12.3456e-35;
+ cMsgAddFloatArray(msg,  "fltA", fvals, 3);
+
+ dvals[0] = -1234567;
+ dvals[1] = +234.678910;
+ dvals[2] = -12.3456789101012e-301;
+ cMsgAddDoubleArray(msg,  "dblA", dvals, 3);
+
+ cMsgAddInt8   (msg, "int8",  SCHAR_MIN);
+ cMsgAddInt16  (msg, "int16", SHRT_MIN);
+ cMsgAddInt32  (msg, "int32", INT_MIN);
+ cMsgAddInt64  (msg, "int64", -9223372036854775807LL);
+
+ cMsgAddUint8   (msg, "uint8",  UCHAR_MAX);
+ cMsgAddUint16  (msg, "uint16", USHRT_MAX);
+ cMsgAddUint32  (msg, "uint32", UINT_MAX);
+ cMsgAddUint64  (msg, "uint64", 18446744073709551615ULL);
+
+ i1vals[0] = -128;
+ i1vals[1] = 127;
+ i1vals[2] = 255;
+ cMsgAddInt8Array(msg, "int8A", i1vals, 3);
+
+ i2vals[0] = -32768;
+ i2vals[1] = 32767;
+ i2vals[2] = 65535;
+ cMsgAddInt16Array(msg, "int16A", i2vals, 3);
+
+ i3vals[0] = -2147483648;
+ i3vals[1] = 2147483647;
+ i3vals[2] = 4294967295;
+ cMsgAddInt32Array(msg, "int32A", i3vals, 3);
+
+ i4vals[0] = -9223372036854775807LL;
+ i4vals[1] = 9223372036854775807LL;
+ i4vals[2] = 18446744073709551615ULL;
+ cMsgAddInt64Array(msg, "int64A", i4vals, 3);
+
+ i5vals[0] = 0;
+ i5vals[1] = 255;
+ i5vals[2] = -1;
+ cMsgAddUint8Array(msg, "uint8A", i5vals, 3);
+
+ i6vals[0] = 0;
+ i6vals[1] = 65535;
+ i6vals[2] = -1;
+ cMsgAddUint16Array(msg, "uint16A", i6vals, 3);
+
+ i7vals[0] = 0U;
+ i7vals[1] = 4294967295U;
+ i7vals[2] = -1;
+ cMsgAddUint32Array(msg, "uint32A", i7vals, 3);
+
+ i8vals[0] = 0ULL;
+ i8vals[1] = 18446744073709551615ULL;
+ i8vals[2] = -1;
+ cMsgAddUint64Array(msg, "uint64A", i8vals, 3);
+
+ /* create message for payload */
+ msg2 = cMsgCreateMessage();    /* allocating mem here */
+ cMsgSetSubject(msg2, subject); /* allocating mem here */
+ cMsgSetType(msg2, type);       /* allocating mem here */
+ cMsgSetText(msg2, "TEXT FIELD IN MSG");       /* allocating mem here */
+ cMsgAddFloat (msg2, "flt", -1.23456e-05);
+ cMsgAddDouble(msg2, "dbl", -1.23456789101012e-195);
+ cMsgAddString (msg2, "str", "HEY, HEY, YOU THERE!!!");
+
+ /* create array of msgs for payload */
+ cMsgAddMessage(msg, "myMessage", msg2);
+ msgs[0] = msg2;
+ msgs[1] = msg2;
+ cMsgAddMessageArray(msg, "msgArray", msgs, 2);
+
+ /*
+  cMsgToString(msg, &p);
+  printf("XML message:\n%s", p);
+  free(p);
+  cMsgGetPayloadText(msg, &p);
+  printf("\n\n\npayload text:\n%s", p);
+ */
+
   while (mainloops-- > 0) {
       count = 0;
       
@@ -276,13 +368,11 @@ printf("Setting UDL = %s\n", UDL);
           /* err = cMsgSyncSend(domainId, msg, NULL, &response); */
           err = cMsgSend(domainId, msg);
           if (err != CMSG_OK) {
-            printf("cMsgSend: err = %d, %s\n",err, cMsgPerror(err));
-            fflush(stdout);
-            goto end;
+              printf("cMsgSend: err = %d, %s\n",err, cMsgPerror(err));
+              fflush(stdout);
+              goto end;
           }
-          cMsgFlush(domainId, NULL);
           count++;
-          
           if (delay != 0) {
               nanosleep(&sleeep, NULL);
           }          
@@ -305,9 +395,10 @@ printf("Setting UDL = %s\n", UDL);
   } 
   
   end:
-printf("producer: will free msg, msg = %p, &msg = %p\n", msg, &msg);  
+          
+  /*printf("producer: will free msg, msg = %p, &msg = %p\n", msg, &msg);*/
   cMsgFreeMessage(&msg);
-printf("producer: will disconnect\n");  
+  /*printf("producer: will disconnect\n");*/  
   err = cMsgDisconnect(&domainId);
   if (err != CMSG_OK) {
       if (debug) {
