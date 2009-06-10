9509ff672d5085de5cc66296f541a8abd570b4b5
Carl Timmer <timmer@jlab.org>
2009-06-10 18:10:41 +0000

    move classes around

--- java/org/jlab/coda/cMsg/apps/cMsgConsumer.java
+++ java/org/jlab/coda/cMsg/apps/cMsgConsumer.java
@@ -17,8 +17,8 @@
 package org.jlab.coda.cMsg.apps;
 
 import org.jlab.coda.cMsg.*;
-import org.jlab.coda.cMsg.common.cMsgCallbackInterface;
-import org.jlab.coda.cMsg.common.cMsgCallbackAdapter;
+import org.jlab.coda.cMsg.cMsgCallbackInterface;
+import org.jlab.coda.cMsg.cMsgCallbackAdapter;
 
 
 /**
