85555db4267b25eefb237e4c789d7a7dd4969b64
Carl Timmer <timmer@jlab.org>
2009-08-14 17:53:54 +0000

    add constructor and javadoc

--- java/org/jlab/coda/cMsg/cMsgException.java
+++ java/org/jlab/coda/cMsg/cMsgException.java
@@ -45,16 +45,22 @@ public class cMsgException extends Exception {
       * Constructs a new exception with the specified detail message and
       * cause.
       *
-      * @param  message the detail message (which is saved for later retrieval
-      *         by the {@link #getMessage()} method).
-      * @param  cause the cause (which is saved for later retrieval by the
-      *         {@link #getCause()} method).  (A <tt>null</tt> value is
-      *         permitted, and indicates that the cause is nonexistent or
-      *         unknown.)
-      * @since  1.4
+     * {@inheritDoc}<p/>
+     *
+     * @param message {@inheritDoc}<p/>
+     * @param cause {@inheritDoc}<p/>
       */
     public cMsgException(String message, Throwable cause) {
         super(message, cause);
+    }
+
+    /**
+     * {@inheritDoc}<p/>
+     *
+     * @param cause {@inheritDoc}<p/>
+      */
+    public cMsgException(Throwable cause) {
+        super(cause);
     }
 
     /**
