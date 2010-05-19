3942352e7ce187d6201231344206ff8d646bef46
Carl Timmer <timmer@jlab.org>
2010-05-19 18:10:38 +0000

    javadoc

--- java/org/jlab/coda/cMsg/cMsgException.java
+++ java/org/jlab/coda/cMsg/cMsgException.java
@@ -6,9 +6,9 @@
  *    described in the NOTICE file included as part of this distribution.     *
  *                                                                            *
  *    Author:  Carl Timmer                                                    *
- *             timmer@jlab.org                   Jefferson Lab, MS-12H        *
+ *             timmer@jlab.org                   Jefferson Lab, MS-12B3       *
  *             Phone: (757) 269-5130             12000 Jefferson Ave.         *
- *             Fax:   (757) 269-5800             Newport News, VA 23606       *
+ *             Fax:   (757) 269-6248             Newport News, VA 23606       *
  *                                                                            *
  *----------------------------------------------------------------------------*/
 
@@ -22,57 +22,91 @@ package org.jlab.coda.cMsg;
 public class cMsgException extends Exception {
     private int returnCode;
 
+    /**
+     * Get the return code specified in the constructor {@link #cMsgException(String, int)}.
+     * @return the return code
+     */
     public int getReturnCode() {
         return returnCode;
     }
 
+    /**
+     * Set the return code.
+     * @param returnCode return code
+     */
     public void setReturnCode(int returnCode) {
         this.returnCode = returnCode;
     }
 
     /**
-     * Constructs a new exception with the specified message and a return code.
+     * Constructs a new exception with the specified detail message and a return code.
+     * The cause is not initialized, and may subsequently be initialized by
+     * a call to {@link #initCause}.
      *
-     * @param msg the error message
+     * @param message the detail message. The detail message is saved for
+     *                later retrieval by the {@link #getMessage()} method.
      * @param returnCode send an integer along with the exception
-     */ 
-    public cMsgException(String msg, int returnCode) {
-        super(msg);
+     */
+    public cMsgException(String message, int returnCode) {
+        super(message);
         this.returnCode = returnCode;
     }
 
     /**
-      * Constructs a new exception with the specified detail message and
-      * cause.
-      *
-     * {@inheritDoc}<p/>
+     * Constructs a new exception with the specified detail message and
+     * cause.  <p>Note that the detail message associated with
+     * <code>cause</code> is <i>not</i> automatically incorporated in
+     * this exception's detail message.
      *
-     * @param message {@inheritDoc}<p/>
-     * @param cause {@inheritDoc}<p/>
-      */
+     * @param message the detail message (which is saved for later retrieval
+     *                by the {@link #getMessage()} method).
+     * @param cause   the cause (which is saved for later retrieval by the
+     *                {@link #getCause()} method).  (A <tt>null</tt> value is
+     *                permitted, and indicates that the cause is nonexistent or
+     *                unknown.)
+     * @since 1.4
+     */
     public cMsgException(String message, Throwable cause) {
         super(message, cause);
     }
 
     /**
-     * {@inheritDoc}<p/>
+     * Constructs a new exception with the specified cause and a detail
+     * message of <tt>(cause==null ? null : cause.toString())</tt> (which
+     * typically contains the class and detail message of <tt>cause</tt>).
+     * This constructor is useful for exceptions that are little more than
+     * wrappers for other throwables (for example, {@link
+     * java.security.PrivilegedActionException}).
      *
-     * @param cause {@inheritDoc}<p/>
-      */
+     * @param  cause the cause (which is saved for later retrieval by the
+     *         {@link #getCause()} method).  (A <tt>null</tt> value is
+     *         permitted, and indicates that the cause is nonexistent or
+     *         unknown.)
+     * @since  1.4
+     */
     public cMsgException(Throwable cause) {
         super(cause);
     }
 
     /**
-     * Constructs a new exception with the specified message.
-     * @param msg the error message
+     * Constructs a new exception with the specified detail message.  The
+     * cause is not initialized, and may subsequently be initialized by
+     * a call to {@link #initCause}.
+     *
+     * @param message the detail message. The detail message is saved for
+     *                later retrieval by the {@link #getMessage()} method.
      */
-    public cMsgException(String msg) {
-        super(msg);
+    public cMsgException(String message) {
+        super(message);
     }
 
-    /** Constructs a new exception with no message. */
+    /**
+     * Constructs a new exception with <code>null</code> as its detail message.
+     * The cause is not initialized, and may subsequently be initialized by a
+     * call to {@link #initCause}.
+     */
     public cMsgException() {
+        super();
     }
 }
 
