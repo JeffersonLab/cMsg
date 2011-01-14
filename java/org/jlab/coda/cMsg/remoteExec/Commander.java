bb21e58b2824b7ac79d099c700ed1dab9bcc4614
Carl Timmer <timmer@jlab.org>
2011-01-14 19:01:23 +0000

    save changes

--- java/org/jlab/coda/cMsg/remoteExec/Commander.java
+++ java/org/jlab/coda/cMsg/remoteExec/Commander.java
@@ -1541,10 +1541,10 @@ System.out.println("Starting Executor with:\n  name = " + arggs[1] + "\n  udl = 
                     try {Thread.sleep(1000);}
                     catch (InterruptedException e) {}
 
-                    //System.out.println("Stop process now");
-                    //cmdr.stop(list.get(0), ret.getId());
-                    System.out.println("StopAll now");
-                    cmdr.stopAll(list.get(0));
+                    System.out.println("Stop process now");
+                    cmdr.stop(list.get(0), ret.getId());
+                    //System.out.println("StopAll now");
+                    //cmdr.stopAll(list.get(0));
 //                    System.out.println("Kill process now");
 //                    cmdr.kill(list.get(0), true);
 
