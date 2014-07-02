7a0d32d422c63b18bd1d546de658e8b246d10e2e
Carl Timmer <timmer@jlab.org>
2014-07-02 20:41:22 +0000

    add emu domain constants

--- java/org/jlab/coda/cMsg/cMsgNetworkConstants.java
+++ java/org/jlab/coda/cMsg/cMsgNetworkConstants.java
@@ -29,7 +29,7 @@ public class cMsgNetworkConstants {
 
     // constants from cMsgNetwork.h
 
-    /** Ints representing ascii for "cMsg is cool", used to filter out portscanning software. */
+    /** Ints representing ascii for "cMsg is cool", used to filter out port-scanning software. */
     public static final int[] magicNumbers = {0x634d7367, 0x20697320, 0x636f6f6c};
 
     /** Multicast address used in runcontrol domain = {@value} */
@@ -38,9 +38,14 @@ public class cMsgNetworkConstants {
     /** Multicast address used in cMsg domain = {@value} */
     public static final String cMsgMulticast = "239.220.0.0";
 
+    /** Multicast address used in Emu domain = {@value} */
+    public static final String emuMulticast = "239.230.0.0";
+
     // There are very few officially used port #s in the 40,000s (only 5),
     // so we'll chose ports in the 45,000 range to avoid potential conflicts.
 
+    /** Default TCP port at which an Emu domain server listens for client connections = {@value} */
+    public static final int    emuTcpPort = 46100;
     /** Default TCP port at which a cMsg domain name server listens for client connections = {@value} */
     public static final int    nameServerTcpPort = 45000;
     /** Default UDP port at which a cMsg name server listens for multicasts = {@value} */
@@ -62,16 +67,26 @@ public class cMsgNetworkConstants {
 
     /** Int to send in UDP packet to cMsg name server if cMsg domain. */
     public static final int    cMsgDomainMulticast = 1;
+
     /** Int to send in UDP packet to server if RC domain and sender is client. */
     public static final int    rcDomainMulticastClient = 2;
-    /** Int to send in UDP packet to rc multicast server if RC domain and sernder is server. */
+    /** Int to send in UDP packet to rc multicast server if RC domain and sender is server. */
     public static final int    rcDomainMulticastServer = 4;
     /** Tell RCMulticast server to kill himself since server at that port & expid already exists. */
     public static final int    rcDomainMulticastKillSelf = 8;
     /** Client is just trying to find RCMulticast servers. */
     public static final int    rcDomainMulticastProbe = 16;
 
-    
+    /** Int to send in UDP packet to server if Emu domain and sender is client. */
+    public static final int    emuDomainMulticastClient = 2;
+    /** Int to send in UDP packet to Emu multicast server if Emu domain and sender is server. */
+    public static final int    emuDomainMulticastServer = 4;
+    /** Tell Emu multicast server to kill himself since server at that port & expid already exists. */
+    public static final int    emuDomainMulticastKillSelf = 8;
+    /** Client is just trying to find Emu multicast servers. */
+    public static final int    emuDomainMulticastProbe = 16;
+
+
     /** The biggest single UDP packet size is 2^16 - IP(v6) 40 byte header - 8 byte UDP header. */
     public static final int    biggestUdpPacketSize = 65487;
 
