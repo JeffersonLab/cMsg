83b1c2e07e1850ff557be3f1b25316f96f73e5b7
Carl Timmer <timmer@jlab.org>
2014-08-15 16:31:11 +0000

    change minor version #

--- src/libsrc/cMsgPrivate.h
+++ src/libsrc/cMsgPrivate.h
@@ -80,7 +80,7 @@ extern "C" {
 /** Major version number. */
 #define CMSG_VERSION_MAJOR 3
 /** Minor version number. */
-#define CMSG_VERSION_MINOR 5
+#define CMSG_VERSION_MINOR 6
 
 /** Default vxworks stack size for subscription threads. */
 #define CMSG_VX_DEFAULT_STACK_SIZE 40000
