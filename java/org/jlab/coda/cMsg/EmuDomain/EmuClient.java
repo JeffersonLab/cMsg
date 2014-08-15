733519f517acbe713856352db5f7bc70d4c52496
Carl Timmer <timmer@jlab.org>
2014-08-15 14:23:48 +0000

    remove comment

--- java/org/jlab/coda/cMsg/EmuDomain/EmuClient.java
+++ java/org/jlab/coda/cMsg/EmuDomain/EmuClient.java
@@ -161,8 +161,6 @@ public class EmuClient extends cMsgDomainAdapter {
         Multicaster sender = new Multicaster(udpPacket);
         sender.start();
 
-        // TODO: make sure sender & receiver are started before waiting for a response?
-
         // wait up to multicast timeout seconds
         boolean response = false;
         if (multicastTimeout > 0) {
