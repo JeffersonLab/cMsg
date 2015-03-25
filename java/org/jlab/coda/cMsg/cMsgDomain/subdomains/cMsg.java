aaeda84dc8da5512f4ef503e7520564e524854fc
Carl Timmer <timmer@jlab.org>
2015-03-25 17:24:00 +0000

    add debug printout

--- java/org/jlab/coda/cMsg/cMsgDomain/subdomains/cMsg.java
+++ java/org/jlab/coda/cMsg/cMsgDomain/subdomains/cMsg.java
@@ -736,6 +736,9 @@ public class cMsg extends cMsgSubdomainAdapter {
                 try {
 //System.out.println("handleSendRequest(subdh): send msg to client " + client.getName());
 //System.out.println("handleSendRequest(subdh): channel = " + client.getMessageChannel());
+                    if (message.getSubject().equalsIgnoreCase("ControlDesigner")) {
+                        System.out.println("cMsg SERVER: send msg w/sub = ControlDesigner");
+                    }
                     client.getDeliverer().deliverMessage(message, cMsgConstants.msgSubscribeResponse);
                 }
                 catch (IOException e) {}
