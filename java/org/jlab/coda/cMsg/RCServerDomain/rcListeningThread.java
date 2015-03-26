ebc9754382ad9dfa0c33baf0cac9a254725374c4
Carl Timmer <timmer@jlab.org>
2015-03-26 18:20:29 +0000

    wait 30 sec for rc client to connect back before returning from this connect

--- java/org/jlab/coda/cMsg/RCServerDomain/rcListeningThread.java
+++ java/org/jlab/coda/cMsg/RCServerDomain/rcListeningThread.java
@@ -33,6 +33,7 @@ import java.nio.charset.Charset;
 import java.util.Date;
 import java.util.Iterator;
 import java.util.Set;
+import java.util.concurrent.CountDownLatch;
 import java.util.concurrent.atomic.AtomicInteger;
 
 /**
@@ -68,6 +69,9 @@ class rcListeningThread extends Thread {
 
     /** Setting this to true will kill this thread. */
     private boolean killThread;
+
+    /** Let a single waiter know that the main thread has been started. */
+    final CountDownLatch startLatch = new CountDownLatch(1);
 
 
 
@@ -429,6 +433,9 @@ class rcListeningThread extends Thread {
                                         tcpBuffer.flip();
                                         dataBuffer = tcpBuffer;
                                         okToParseMsg = true;
+                                        // TCP connection established from RC client,
+                                        // RC server connect() can now return.
+                                        startLatch.countDown();
                                     }
                                 }
                             }
